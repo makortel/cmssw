0c7cc37d97bf98e82a24876c22460fbdd2d77a4b
Matti Kortelainen <matti.kortelainen@cern.ch>
2013-08-30 08:39:06 +0200

    Fix L1-offline matching problem in EventsPerFilter histogram
    Backported from CMSSW_7_0_X commit 210f213

--- DQMOffline/Trigger/interface/HLTTauDQMPath.h
+++ DQMOffline/Trigger/interface/HLTTauDQMPath.h
@@ -54,7 +54,10 @@ public:
 
   size_t filtersSize() const { return filterIndices_.size(); }
   const std::string& getFilterName(size_t i) const { return std::get<0>(filterIndices_[i]); }
+
+  bool isFirstFilterL1Seed() const { return isFirstL1Seed_; }
   const std::string& getLastFilterName() const { return std::get<0>(filterIndices_.back()); }
+
   // index (to edm::TriggerResults) of a filter
   size_t getFilterIndex(size_t i) const { return std::get<1>(filterIndices_[i]); }
 
@@ -62,7 +65,7 @@ public:
   void getFilterObjects(const trigger::TriggerEvent& triggerEvent, size_t i, std::vector<Object>& retval) const;
 
   // i = filter index
-  bool offlineMatching(size_t i, const std::vector<Object>& triggerObjects, const std::map<int, LVColl>& offlineObjects, double dR) const;
+  bool offlineMatching(size_t i, const std::vector<Object>& triggerObjects, const std::map<int, LVColl>& offlineObjects, double dR, std::vector<Object>& matchedTriggerObjects, LVColl& matchedOfflineObjects) const;
 
   typedef std::tuple<std::string, size_t> FilterIndex;
 private:
@@ -78,6 +81,7 @@ private:
   std::vector<int> filterLeptonN_;
   std::string pathName_;
   unsigned int pathIndex_;
+  bool isFirstL1Seed_;
 };
 
 
