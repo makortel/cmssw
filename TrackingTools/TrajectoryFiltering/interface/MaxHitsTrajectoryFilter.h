4eced4912566199a073a591349e5ae1c40df78ce
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-01-31 10:54:52 +0100

    Convert TrajectoryFilter classes to be constructed via plugin factory
    - Add TrajectoryFilter::setEvent() to read data from edm::Event and/or edm::EventSetup
    - Remove TrajectoryFilter ESProducers

--- TrackingTools/TrajectoryFiltering/interface/MaxHitsTrajectoryFilter.h
+++ TrackingTools/TrajectoryFiltering/interface/MaxHitsTrajectoryFilter.h
@@ -11,7 +11,6 @@ public:
   explicit MaxHitsTrajectoryFilter(const edm::ParameterSet & pset):
     theMaxHits( pset.getParameter<int>("maxNumberOfHits")) {}
 
-  
   virtual bool qualityFilter( const Trajectory& traj) const { return TrajectoryFilter::qualityFilterIfNotContributing; }
   virtual bool qualityFilter( const TempTrajectory& traj) const { return TrajectoryFilter::qualityFilterIfNotContributing; }
 
