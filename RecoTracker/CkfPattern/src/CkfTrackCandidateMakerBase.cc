a4a7bbd0c07a7b188e38effcdefa684366f939f0
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-02-06 08:56:52 +0100

    Remove CkfTrackCandidataMakerBase::conf_ member

--- RecoTracker/CkfPattern/src/CkfTrackCandidateMakerBase.cc
+++ RecoTracker/CkfPattern/src/CkfTrackCandidateMakerBase.cc
@@ -52,8 +52,6 @@ namespace {
 
 namespace cms{
   CkfTrackCandidateMakerBase::CkfTrackCandidateMakerBase(edm::ParameterSet const& conf, edm::ConsumesCollector && iC) : 
-
-    conf_(conf),
     theTrackCandidateOutput(true),
     theTrajectoryOutput(false),
     useSplitting(conf.getParameter<bool>("useHitsSplitting")),
@@ -65,6 +63,7 @@ namespace cms{
     theTrajectoryCleanerName(conf.getParameter<std::string>("TrajectoryCleaner")), 
     theTrajectoryCleaner(0),
     theInitialState(new TransientInitialStateEstimator(conf.getParameter<ParameterSet>("TransientInitialStateEstimatorParameters"))),
+    theMagFieldName(conf.exists("SimpleMagneticField") ? conf.getParameter<std::string>("SimpleMagneticField") : ""),
     theNavigationSchoolName(conf.getParameter<std::string>("NavigationSchool")),
     theNavigationSchool(0),
     theSeedCleaner(0),
@@ -88,7 +87,7 @@ namespace cms{
         maskStripsLazy_ = iC.consumes<StripClusterLazyMask>(conf.getParameter<edm::InputTag>("clustersToSkip"));
       }
 
-    std::string cleaner = conf_.getParameter<std::string>("RedundantSeedCleaner");
+    std::string cleaner = conf.getParameter<std::string>("RedundantSeedCleaner");
     if (cleaner == "SeedCleanerByHitPosition") {
         theSeedCleaner = new SeedCleanerByHitPosition();
     } else if (cleaner == "SeedCleanerBySharedInput") {
@@ -96,10 +95,10 @@ namespace cms{
     } else if (cleaner == "CachingSeedCleanerByHitPosition") {
         theSeedCleaner = new CachingSeedCleanerByHitPosition();
     } else if (cleaner == "CachingSeedCleanerBySharedInput") {
-      int numHitsForSeedCleaner = conf_.existsAs<int>("numHitsForSeedCleaner") ? 
-	conf_.getParameter<int>("numHitsForSeedCleaner") : 4;
-      int onlyPixelHits = conf_.existsAs<bool>("onlyPixelHitsForSeedCleaner") ? 
-	conf_.getParameter<bool>("onlyPixelHitsForSeedCleaner") : false;
+      int numHitsForSeedCleaner = conf.existsAs<int>("numHitsForSeedCleaner") ?
+	conf.getParameter<int>("numHitsForSeedCleaner") : 4;
+      int onlyPixelHits = conf.existsAs<bool>("onlyPixelHitsForSeedCleaner") ?
+	conf.getParameter<bool>("onlyPixelHitsForSeedCleaner") : false;
       theSeedCleaner = new CachingSeedCleanerBySharedInput(numHitsForSeedCleaner,onlyPixelHits);
     } else if (cleaner == "none") {
         theSeedCleaner = 0;
@@ -125,10 +124,7 @@ namespace cms{
 
     //services
     es.get<TrackerRecoGeometryRecord>().get( theGeomSearchTracker );
-    std::string mfName = "";
-    if (conf_.exists("SimpleMagneticField"))
-      mfName = conf_.getParameter<std::string>("SimpleMagneticField");
-    es.get<IdealMagneticFieldRecord>().get(mfName,theMagField );
+    es.get<IdealMagneticFieldRecord>().get(theMagFieldName, theMagField );
     //    edm::ESInputTag mfESInputTag(mfName);
     //    es.get<IdealMagneticFieldRecord>().get(mfESInputTag,theMagField );
 
