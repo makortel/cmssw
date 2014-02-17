4b78ef9faffa2614eb0dc9e021bfefe6b93ad8f7
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-02-17 15:28:26 +0100

    Remove inheritance from HitPairGenerator from HitQuadrupletGeneratorFromLayerPairForPhotonConversion
    HitQuadrupletGeneratorFromLayerPairForPhotonConversion does not really
    benefit from the base classes, and the only user
    (CombinedHitQuadrupletGeneratorForPhotonConversion) uses the type
    explicitly, so there is no need for dynamic polymorphism either. In
    fact, now we can remove the non-implemented methods that are pure
    virtual in the base class.

--- RecoTracker/ConversionSeedGenerators/interface/HitQuadrupletGeneratorFromLayerPairForPhotonConversion.h
+++ RecoTracker/ConversionSeedGenerators/interface/HitQuadrupletGeneratorFromLayerPairForPhotonConversion.h
@@ -1,7 +1,6 @@
 #ifndef HitQuadrupletGeneratorFromLayerPairForPhotonConversion_h
 #define HitQuadrupletGeneratorFromLayerPairForPhotonConversion_h
 
-#include "RecoTracker/TkHitPairs/interface/HitPairGenerator.h"
 #include "RecoTracker/TkHitPairs/interface/CombinedHitPairGenerator.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 
@@ -10,7 +9,7 @@
 class DetLayer;
 class TrackingRegion;
 
-class HitQuadrupletGeneratorFromLayerPairForPhotonConversion : public HitPairGenerator {
+class HitQuadrupletGeneratorFromLayerPairForPhotonConversion {
 
 public:
 
@@ -21,22 +20,13 @@ public:
   HitQuadrupletGeneratorFromLayerPairForPhotonConversion(unsigned int inner,
                                 unsigned int outer,
 				LayerCacheType* layerCache,
-				unsigned int nSize=30000,
 				unsigned int max=0);
 
   virtual ~HitQuadrupletGeneratorFromLayerPairForPhotonConversion() { }
 
-  void setSeedingLayers(Layers layers) override { theSeedingLayers = layers; }
-
   virtual void hitPairs( const TrackingRegion& reg, OrderedHitPairs & prs, 
+                         const Layers& layers,
 			 const edm::Event & ev,  const edm::EventSetup& es);
-
-  virtual HitQuadrupletGeneratorFromLayerPairForPhotonConversion* clone() const {
-    return new HitQuadrupletGeneratorFromLayerPairForPhotonConversion(*this);
-  }
-
-  Layer innerLayer() const { return theSeedingLayers[theInnerLayer]; }
-  Layer outerLayer() const { return theSeedingLayers[theOuterLayer]; }
 
   bool failCheckRZCompatibility(const RecHitsSortedInPhi::Hit & hit, const DetLayer& layer, const HitRZCompatibility *checkRZ, const TrackingRegion & region);
   //void checkPhiRange(double phi1, double phi2);
@@ -54,9 +44,9 @@ public:
 private:
   
   LayerCacheType & theLayerCache;
-  Layers theSeedingLayers;
   const unsigned int theOuterLayer;
   const unsigned int theInnerLayer;
+  const unsigned int theMaxElement;
 
   std::stringstream *ss;
 
