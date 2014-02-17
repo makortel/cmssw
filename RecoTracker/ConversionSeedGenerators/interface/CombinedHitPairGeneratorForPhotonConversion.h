8cd93415aacfc9b6b272b122c1e557a2a0fe014c
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-02-17 14:57:58 +0100

    Remove inheritance of HitPairGenerator from CombinedHitPairGeneratorForPhotonConversion
    CombinedHitPairGeneratorForPhotonConversion does not really benefit
    from the base classes, and the user
    (PhotonConversionTrajectorySeedProducerFromSingleLegAlgo) uses the
    concrete type explicitly, so no need for dynamic polymorphism either.
    In fact, now we can remove the non-implemented methods that are pure
    virtual in the base class.

--- RecoTracker/ConversionSeedGenerators/interface/CombinedHitPairGeneratorForPhotonConversion.h
+++ RecoTracker/ConversionSeedGenerators/interface/CombinedHitPairGeneratorForPhotonConversion.h
@@ -3,9 +3,8 @@
 
 #include <vector>
 #include <memory>
-#include "RecoTracker/TkHitPairs/interface/HitPairGenerator.h"
+#include "RecoTracker/TkHitPairs/interface/OrderedHitPairs.h"
 #include "RecoTracker/TkHitPairs/interface/LayerHitMapCache.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Utilities/interface/EDGetToken.h"
 
 
@@ -13,7 +12,7 @@ class TrackingRegion;
 class OrderedHitPairs;
 class HitPairGeneratorFromLayerPairForPhotonConversion;
 class SeedingLayerSetsHits;
-namespace edm { class Event; class EventSetup; }
+namespace edm { class Event; class EventSetup; class ParameterSet; class ConsumesCollector;}
 
 #include "RecoTracker/ConversionSeedGenerators/interface/ConversionRegion.h"
 
@@ -21,22 +20,13 @@ namespace edm { class Event; class EventSetup; }
  * Hides set of HitPairGeneratorFromLayerPairForPhotonConversion generators.
  */
 
-class CombinedHitPairGeneratorForPhotonConversion : public HitPairGenerator {
+class CombinedHitPairGeneratorForPhotonConversion {
 public:
   typedef LayerHitMapCache LayerCacheType;
 
 public:
   CombinedHitPairGeneratorForPhotonConversion(const edm::ParameterSet & cfg, edm::ConsumesCollector& iC);
-  virtual ~CombinedHitPairGeneratorForPhotonConversion();
-
-  void setSeedingLayers(SeedingLayerSetsHits::SeedingLayerSet layers) override;
-
-  /// form base class
-  virtual void hitPairs(const TrackingRegion&, OrderedHitPairs&, const edm::Event&, const edm::EventSetup&){};
-
-  /// from base class
-  virtual CombinedHitPairGeneratorForPhotonConversion * clone() const 
-    { return new CombinedHitPairGeneratorForPhotonConversion(*this); }
+  ~CombinedHitPairGeneratorForPhotonConversion();
 
   /*Added to the original class*/
   const OrderedHitPairs & run(
@@ -51,9 +41,8 @@ public:
 
   /*------------------------*/
 private:
-  CombinedHitPairGeneratorForPhotonConversion(const CombinedHitPairGeneratorForPhotonConversion & cb); 
-
   edm::EDGetTokenT<SeedingLayerSetsHits> theSeedingLayerToken;
+  const unsigned int theMaxElement;
   uint32_t maxHitPairsPerTrackAndGenerator;
 
   LayerCacheType   theLayerCache;
