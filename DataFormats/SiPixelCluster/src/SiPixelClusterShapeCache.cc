9b31cbf28296664e702489c799ec71c7074524c6
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-02-28 08:37:57 +0100

    Convert couple of asserts to cms::Exceptions in SiPixelClusterShapeCache

--- DataFormats/SiPixelCluster/src/SiPixelClusterShapeCache.cc
+++ DataFormats/SiPixelCluster/src/SiPixelClusterShapeCache.cc
@@ -1,4 +1,5 @@
 #include "DataFormats/SiPixelCluster/interface/SiPixelClusterShapeCache.h"
+#include "FWCore/Utilities/interface/Exception.h"
 
 SiPixelClusterShapeData::~SiPixelClusterShapeData() {}
 
@@ -6,3 +7,13 @@ SiPixelClusterShapeCache::LazyGetter::LazyGetter() {}
 SiPixelClusterShapeCache::LazyGetter::~LazyGetter() {}
 
 SiPixelClusterShapeCache::~SiPixelClusterShapeCache() {}
+
+void SiPixelClusterShapeCache::check_productId(edm::ProductID id) const {
+  if(id != productId_)
+    throw cms::Exception("InvalidReference") << "SiPixelClusterShapeCache caches values for SiPixelClusters with ProductID " << productId_ << ", got SiPixelClusterRef with ID " << id;
+}
+
+void SiPixelClusterShapeCache::check_index(ClusterRef::key_type index) const {
+  if(index >= data_.size())
+    throw cms::Exception("InvalidReference") << "SiPixelClusterShapeCache caches values for SiPixelClusters with ProductID " << productId_ << " that has " << data_.size() << " clusters, got SiPixelClusterRef with index " << index;
+}
