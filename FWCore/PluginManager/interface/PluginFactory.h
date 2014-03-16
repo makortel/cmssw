e09998324d89cfe0c32e2b11d2a40d9d4b5c6267
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-03-16 09:00:06 +0100

    First step for supporting Service::fillDescriptions()

--- FWCore/PluginManager/interface/PluginFactory.h
+++ FWCore/PluginManager/interface/PluginFactory.h
@@ -19,6 +19,7 @@
 //
 
 // system include files
+#include <functional>
 #include <map>
 #include <vector>
 
@@ -26,11 +27,39 @@
 #include "FWCore/PluginManager/interface/PluginFactoryBase.h"
 #include "FWCore/PluginManager/interface/PluginManager.h"
 // forward declarations
+namespace edm {
+  class ConfigurationDescriptions;
+}
 
 namespace edmplugin {
 template< class T> class PluginFactory;
   class DummyFriend;
   
+  namespace impl {
+    // http://stackoverflow.com/questions/9530928/checking-a-member-exists-possibly-in-a-base-class-c11-versio
+    // http://stackoverflow.com/questions/257288/is-it-possible-to-write-a-c-template-to-check-for-a-functions-existence/264088
+    template <typename TPlug, int>
+    constexpr auto has_fillDescriptions(edm::ConfigurationDescriptions& descriptions) -> decltype(TPlug::fillDescriptions(descriptions), bool()) {
+      return true;
+    }
+    template <typename TPlug, long>
+    constexpr bool has_fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+      return false;
+    }
+    template <typename TPlug, bool>
+    struct CallFillDescriptions {
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions, std::function<void(edm::ConfigurationDescriptions&)> defaultFunction) {
+        TPlug::fillDescriptions(descriptions);
+      }
+    };
+    template <typename TPlug>
+    struct CallFillDescriptions<TPlug, false> {
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions, std::function<void(edm::ConfigurationDescriptions&)> defaultFunction) {
+        defaultFunction(descriptions);
+      }
+    };
+  }
+
 template<typename R, typename... Args>
 class PluginFactory<R*(Args...)> : public PluginFactoryBase
 {
@@ -40,16 +69,25 @@ class PluginFactory<R*(Args...)> : public PluginFactoryBase
       typedef R* ReturnType;
 
       struct PMakerBase {
+        typedef std::function<void(edm::ConfigurationDescriptions&)> CallbackType;
         virtual R* create(Args...) const = 0;
+        virtual void fillDescriptions(edm::ConfigurationDescriptions& descriptions, CallbackType defaultFunction, CallbackType prevalidateFunction) const = 0;
         virtual ~PMakerBase() {}
       };
       template<class TPlug>
       struct PMaker : public PMakerBase {
+        typedef typename PMakerBase::CallbackType CallbackType;
         PMaker(const std::string& iName) {
           PluginFactory<R*(Args...)>::get()->registerPMaker(this,iName);
         }
         virtual R* create(Args... args) const {
           return new TPlug(std::forward<Args>(args)...);
+        }
+        // dirty solution with callbacks in order to avoid circular
+        // dependence between PluginManager and ParameterSet packages
+        virtual void fillDescriptions(edm::ConfigurationDescriptions& descriptions, CallbackType defaultFunction, CallbackType prevalidateFunction) const override {
+          impl::CallFillDescriptions<TPlug, impl::has_fillDescriptions<TPlug, 0>(descriptions)>::fillDescriptions(descriptions, defaultFunction);
+          prevalidateFunction(descriptions);
         }
       };
 
