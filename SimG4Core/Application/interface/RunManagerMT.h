018611f9334a664cd8d37da32d87c33f989b8b29
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-06-08 17:02:32 +0200

    First bunch of configuration stuff to RunManagerMT

--- SimG4Core/Application/interface/RunManagerMT.h
+++ SimG4Core/Application/interface/RunManagerMT.h
@@ -3,11 +3,33 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+#include <string>
+#include <vector>
+
 class RunManagerMT {
 public:
   RunManagerMT(const edm::ParameterSet& iConfig);
   ~RunManagerMT();
+
 private:
+  const bool m_nonBeam;
+  const bool m_pUseMagneticField;
+  const std::string m_PhysicsTablesDir;
+  const bool m_StorePhysicsTables;
+  const bool m_RestorePhysicsTables;
+  const int m_EvtMgrVerbosity;
+  const edm::ParameterSet m_pField;
+  const edm::ParameterSet m_pGenerator;
+  const edm::ParameterSet m_pPhysics;
+  const edm::ParameterSet m_pRunAction;
+  const edm::ParameterSet m_pEventAction;
+  const edm::ParameterSet m_pStackingAction;
+  const edm::ParameterSet m_pTrackingAction;
+  const edm::ParameterSet m_pSteppingAction;
+  const std::vector<std::string> m_G4Commands;
+  //edm::ParameterSet m_p;
+
+  edm::InputTag m_theLHCTlinkTag;
 };
 
 #endif
--- SimG4Core/Application/src/RunManagerMT.cc
+++ SimG4Core/Application/src/RunManagerMT.cc
@@ -1,6 +1,24 @@
 #include "SimG4Core/Application/interface/RunManagerMT.h"
 
-RunManagerMT::RunManagerMT(const edm::ParameterSet& iConfig) {
+RunManagerMT::RunManagerMT(const edm::ParameterSet& iConfig):
+  m_nonBeam(iConfig.getParameter<bool>("NonBeamEvent")),
+  m_pUseMagneticField(iConfig.getParameter<bool>("UseMagneticField")),
+  m_PhysicsTablesDir(iConfig.getParameter<std::string>("PhysicsTablesDirectory")),
+  m_StorePhysicsTables(iConfig.getParameter<bool>("StorePhysicsTables")),
+  m_RestorePhysicsTables(iConfig.getParameter<bool>("RestorePhysicsTables")),
+  m_EvtMgrVerbosity(iConfig.getUntrackedParameter<int>("G4EventManagerVerbosity",0)),
+  m_pField(iConfig.getParameter<edm::ParameterSet>("MagneticField")),
+  m_pGenerator(iConfig.getParameter<edm::ParameterSet>("Generator")),
+  m_pPhysics(iConfig.getParameter<edm::ParameterSet>("Physics")),
+  m_pRunAction(iConfig.getParameter<edm::ParameterSet>("RunAction")),
+  m_pEventAction(iConfig.getParameter<edm::ParameterSet>("EventAction")),
+  m_pStackingAction(iConfig.getParameter<edm::ParameterSet>("StackingAction")),
+  m_pTrackingAction(iConfig.getParameter<edm::ParameterSet>("TrackingAction")),
+  m_pSteppingAction(iConfig.getParameter<edm::ParameterSet>("SteppingAction")),
+  m_G4Commands(iConfig.getParameter<std::vector<std::string> >("G4Commands")),
+//m_p(p)
+  m_theLHCTlinkTag(iConfig.getParameter<edm::InputTag>("theLHCTlinkTag"))
+{
 }
 
 RunManagerMT::~RunManagerMT() {}
