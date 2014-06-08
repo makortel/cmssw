439007e082b6320eb5db979e6d90caa86c2efd61
Matti Kortelainen <matti.kortelainen@cern.ch>
2014-06-08 12:18:02 +0200

    Using both global and run cache
    I feel for Geant4 we need both

--- SimG4Core/Application/plugins/OscarMTTest.cc
+++ SimG4Core/Application/plugins/OscarMTTest.cc
@@ -21,32 +21,43 @@ namespace {
     return sum;
   }
 
-  class ThreadWork {
+  class Configuration {
   public:
-    ThreadWork(const edm::ParameterSet& iConfig):
+    Configuration(const edm::ParameterSet& iConfig):
       m_id(iConfig.getParameter<int>("id"))
     {}
 
     int id() const { return m_id; }
+
+  private:
+    int m_id;
+  };
+
+
+  class ThreadWork {
+  public:
+    ThreadWork(const Configuration *config):
+      m_config(config)
+    {}
 
     std::mutex& mutex() { return m_mutex; }
     std::condition_variable& cv() { return m_cv; }
 
     void run() {
       {
-        edm::LogPrint("Test") << "Id " << m_id << " thread " << std::this_thread::get_id();
+        edm::LogPrint("Test") << "Id " << m_config->id() << " thread " << std::this_thread::get_id();
         std::lock_guard<std::mutex> lk(m_mutex);
-        edm::LogPrint("Test") << "Id " << m_id << " thread " << std::this_thread::get_id() << " got lock";
+        edm::LogPrint("Test") << "Id " << m_config->id() << " thread " << std::this_thread::get_id() << " got lock";
         double sum = doWork();
-        edm::LogPrint("Test") << "Id " << m_id << " thread " << std::this_thread::get_id() << " sum " << sum;
+        edm::LogPrint("Test") << "Id " << m_config->id() << " thread " << std::this_thread::get_id() << " sum " << sum;
       }
       m_cv.notify_one();
-      edm::LogPrint("Test") << "Id " << m_id << " thread " << std::this_thread::get_id() << " cv signaled";
+      edm::LogPrint("Test") << "Id " << m_config->id() << " thread " << std::this_thread::get_id() << " cv signaled";
     }
   private:
     std::mutex m_mutex;
     std::condition_variable m_cv;
-    const int m_id;
+    const Configuration *m_config;
   };
 
   class MasterThread {
@@ -71,29 +82,37 @@ namespace {
   };
 
   class OscarMTTest: public edm::stream::EDProducer<
-    edm::GlobalCache<MasterThread>
+    edm::GlobalCache<Configuration>,
+    edm::RunCache<MasterThread>
     > {
   public:
-    explicit OscarMTTest(const edm::ParameterSet& iConfig, const MasterThread *masterThread) {}
+    explicit OscarMTTest(const edm::ParameterSet& iConfig, const Configuration *config) {}
     virtual ~OscarMTTest() {}
 
-    static std::unique_ptr<MasterThread> initializeGlobalCache(const edm::ParameterSet& iConfig) {
-      auto work = std::make_shared<ThreadWork>(iConfig);
+    static std::unique_ptr<Configuration> initializeGlobalCache(const edm::ParameterSet& iConfig) {
+      return std::unique_ptr<Configuration>(new Configuration(iConfig));
+    }
+
+    static std::shared_ptr<MasterThread> globalBeginRun(const edm::Run& iRun, const edm::EventSetup& iSetup, const GlobalCache *globalCache) {
+      auto work = std::make_shared<ThreadWork>(globalCache);
 
       std::unique_lock<std::mutex> lk(work->mutex());
-      edm::LogPrint("Test") << "Id " << work->id() << " initializeGlobalCache(): created lock";
+      edm::LogPrint("Test") << "Id " << globalCache->id() << " globalBeginRun(): created lock";
 
-      auto masterThread = std::unique_ptr<MasterThread>(new MasterThread(work));
-      edm::LogPrint("Test") << "Id " << work->id() << " initializeGlobalCache(): Created MasterThread, waiting on cv";
+      auto masterThread = std::make_shared<MasterThread>(work);
+      edm::LogPrint("Test") << "Id " << globalCache->id() << " globalBeginRun(): Created MasterThread, waiting on cv";
       work->cv().wait(lk);
-      edm::LogPrint("Test") << "Id " << work->id() << " initializeGlobalCache(): Wait finished";
+      edm::LogPrint("Test") << "Id " << globalCache->id() << " globalBeginRun(): Wait finished";
       lk.unlock();
 
       return masterThread;
     }
 
-    static void globalEndJob(MasterThread *master) {
-      edm::LogPrint("Test") << "Id " << master->threadWork().id() << " number of events seen " << master->count;
+    static void globalEndRun(const edm::Run& iRun, const edm::EventSetup& iSetup, const RunContext *runContext) {
+      edm::LogPrint("Test") << "Id " << runContext->global()->id() << " number of events seen " << runContext->run()->count;
+    }
+
+    static void globalEndJob(Configuration *config) {
     }
 
     virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) override;
@@ -103,11 +122,11 @@ namespace {
 
 
   void OscarMTTest::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    ++(globalCache()->count);
+    ++(runCache()->count);
 
     double sum = doWork();
 
-    edm::LogPrint("Test") << "Id " << globalCache()->threadWork().id()
+    edm::LogPrint("Test") << "Id " << globalCache()->id()
                           << " event " << iEvent.id().event() 
                           << " stream " << iEvent.streamID().value()
                           << " " << sum;
