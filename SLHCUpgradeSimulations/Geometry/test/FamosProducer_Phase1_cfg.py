e75559af2b7cf4839d66fcea2e77a84180e4338c
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-03-17 10:42:45 +0100

    Migrate other uses of MTV

--- SLHCUpgradeSimulations/Geometry/test/FamosProducer_Phase1_cfg.py
+++ SLHCUpgradeSimulations/Geometry/test/FamosProducer_Phase1_cfg.py
@@ -120,7 +120,12 @@ process.multiTrackValidator.label = ['ctfWithMaterialTracks']
 #process.multiTrackValidator.label_tp_effic = cms.InputTag("cutsTPEffic")
 #process.multiTrackValidator.label_tp_fake = cms.InputTag("cutsTPFake")
 process.multiTrackValidator.sim = 'famosSimHits'
-process.multiTrackValidator.associators = ['TrackAssociatorByHits']
+process.multiTrackValidator.associators = [
+    cms.PSet(
+        src = cms.InputTag('TrackAssociatorByHits'),
+        label = cms.string('TrackAssociatorByHits'),
+    )
+]
 process.multiTrackValidator.UseAssociators = True
 process.multiTrackValidator.outputFile = "validphase1_muon_50GeV.root"
 process.multiTrackValidator.nint = cms.int32(20)
