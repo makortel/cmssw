062f733df1161008a5667ec096182c0a34518f69
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-03-20 14:03:54 +0100

    Specialize

--- CommonTools/Utils/interface/associationMapFilterValues.h
+++ CommonTools/Utils/interface/associationMapFilterValues.h
@@ -1,18 +1,41 @@
 #ifndef CommonTools_Utils_associationMapFilterValues_h
 #define CommonTools_Utils_associationMapFilterValues_h
 
+#include "DataFormats/Common/interface/Ref.h"
+
 #include <unordered_set>
 
 namespace associationMapFilterValuesHelpers {
+  // Common implementation for Ref and RefToBase
+  template <typename T_AssociationMap, typename T_KeyValue, typename T_ValueIndices>
+  void ref_insert(T_AssociationMap& ret, const T_KeyValue& keyValue, const T_ValueIndices& value_indices) {
+    if(value_indices.find(keyValue.val.key()) != value_indices.end()) {
+      ret.insert(keyValue);
+    }
+  }
+
+  // By default no implementation, as it turns out to be very specific for the types
   template <typename DataTag>
-  struct IfFound {
+  struct IfFound;
+
+  // Specialize for Ref and RefToBase, implementation is the same
+  template <typename C, typename T, typename F>
+  struct IfFound<edm::Ref<C, T, F>> {
     template <typename T_AssociationMap, typename T_KeyValue, typename T_ValueIndices>
     static void insert(T_AssociationMap& ret, const T_KeyValue& keyValue, const T_ValueIndices& value_indices) {
-      if(value_indices.find(keyValue.val.key()) != value_indices.end()) {
-        ret.insert(keyValue);
-      }
+      ref_insert(ret, keyValue, value_indices);
     }
   };
+
+  template <typename T>
+  struct IfFound<edm::RefToBase<T>> {
+    template <typename T_AssociationMap, typename T_KeyValue, typename T_ValueIndices>
+    static void insert(T_AssociationMap& ret, const T_KeyValue& keyValue, const T_ValueIndices& value_indices) {
+      ref_insert(ret, keyValue, value_indices);
+    }
+  };
+
+  //
 }
 
 template <typename T_AssociationMap, typename T_RefVector>
