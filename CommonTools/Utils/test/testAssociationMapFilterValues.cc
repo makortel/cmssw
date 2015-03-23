a859bc9a82f7211d5cfd621f62338a8320ccc764
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-03-23 09:37:36 +0100

    Support OneToManyWithQuality

--- CommonTools/Utils/test/testAssociationMapFilterValues.cc
+++ CommonTools/Utils/test/testAssociationMapFilterValues.cc
@@ -7,6 +7,7 @@ class testAssociationMapFilterValues : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(testAssociationMapFilterValues);
   CPPUNIT_TEST(checkOneToOne);
   CPPUNIT_TEST(checkOneToMany);
+  CPPUNIT_TEST(checkOneToManyQuality);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -14,6 +15,7 @@ public:
   void tearDown() {}
   void checkOneToOne();
   void checkOneToMany();
+  void checkOneToManyQuality();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION( testAssociationMapFilterValues );
@@ -122,6 +124,7 @@ void testAssociationMapFilterValues::checkOneToMany() {
     CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 0)) != filtered.end() );
     CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 1)) == filtered.end() );
     CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 2)) != filtered.end() );
+    CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 3)) != filtered.end() );
   }
 
   // RefToBase data
@@ -157,5 +160,120 @@ void testAssociationMapFilterValues::checkOneToMany() {
     CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 0)) != filtered.end() );
     CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 1)) == filtered.end() );
     CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 2)) != filtered.end() );
+    CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 3)) != filtered.end() );
+  }
+}
+
+void testAssociationMapFilterValues::checkOneToManyQuality() {
+  // simple data
+  {
+    typedef std::vector<int> CKey;
+    typedef std::vector<double> CVal;
+    typedef edm::AssociationMap<edm::OneToManyWithQuality<CKey, CVal, double, unsigned char> > Assoc;
+
+    CKey keys{1, 2, 3};
+    CVal values{1.0, 2.0, 3.0, 4.0};
+
+    Assoc map;
+    map.insert(edm::Ref<CKey>(&keys, 0), std::make_pair(edm::Ref<CVal>(&values, 0), 0.1));
+    map.insert(edm::Ref<CKey>(&keys, 1), std::make_pair(edm::Ref<CVal>(&values, 1), 0.2));
+    map.insert(edm::Ref<CKey>(&keys, 2), std::make_pair(edm::Ref<CVal>(&values, 2), 0.3));
+    map.insert(edm::Ref<CKey>(&keys, 2), std::make_pair(edm::Ref<CVal>(&values, 3), 0.4));
+
+    std::vector<edm::Ref<CVal>> keep{edm::Ref<CVal>(&values, 0), edm::Ref<CVal>(&values, 2)};
+
+    Assoc filtered = associationMapFilterValues(map, keep);
+    CPPUNIT_ASSERT( filtered.size() == 2 );
+    auto found = filtered.find(edm::Ref<CKey>(&keys, 0));
+    CPPUNIT_ASSERT( found != filtered.end() );
+    CPPUNIT_ASSERT( found->val.size() == 1 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.1 );
+    CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 1)) == filtered.end() );
+    found = filtered.find(edm::Ref<CKey>(&keys, 2));
+    CPPUNIT_ASSERT( found->val.size() == 1 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.3 );
+  }
+
+  // Ref data
+  {
+    typedef std::vector<int> CKey;
+    typedef std::vector<double> Data;
+    typedef std::vector<edm::Ref<Data>> CVal;
+    typedef edm::AssociationMap<edm::OneToManyWithQuality<CKey, CVal, double, unsigned char> > Assoc;
+
+    CKey keys{1, 2, 3};
+    Data data{1.0, 2.0, 3.0, 4.0};
+    CVal values{edm::Ref<Data>(&data, 0), edm::Ref<Data>(&data, 1), edm::Ref<Data>(&data, 2)};
+
+    Assoc map;
+    map.insert(edm::Ref<CKey>(&keys, 0), std::make_pair(edm::Ref<CVal>(&values, 0), 0.1));
+    map.insert(edm::Ref<CKey>(&keys, 1), std::make_pair(edm::Ref<CVal>(&values, 1), 0.2));
+    map.insert(edm::Ref<CKey>(&keys, 2), std::make_pair(edm::Ref<CVal>(&values, 1), 0.3));
+    map.insert(edm::Ref<CKey>(&keys, 2), std::make_pair(edm::Ref<CVal>(&values, 0), 0.4));
+    map.insert(edm::Ref<CKey>(&keys, 3), std::make_pair(edm::Ref<CVal>(&values, 2), 0.5));
+
+    std::vector<edm::Ref<CVal>> keep{edm::Ref<CVal>(&values, 0), edm::Ref<CVal>(&values, 1)};
+
+    Assoc filtered = associationMapFilterValues(map, keep);
+    CPPUNIT_ASSERT( filtered.size() == 3 );
+    auto found = filtered.find(edm::Ref<CKey>(&keys, 0));
+    CPPUNIT_ASSERT( found != filtered.end() );
+    CPPUNIT_ASSERT( found->val.size() == 1 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.1 );
+    found = filtered.find(edm::Ref<CKey>(&keys, 1));
+    CPPUNIT_ASSERT( found != filtered.end() );
+    CPPUNIT_ASSERT( found->val.size() == 1 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.2 );
+    found = filtered.find(edm::Ref<CKey>(&keys, 2));
+    CPPUNIT_ASSERT( found != filtered.end() );
+    CPPUNIT_ASSERT( found->val.size() == 2 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.3 );
+    CPPUNIT_ASSERT( found->val[1].second == 0.4 );
+    CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 3)) == filtered.end() );
+  }
+
+  // RefToBase data
+  {
+    typedef std::vector<int> CKey;
+    typedef std::vector<Derived> Data;
+    typedef std::vector<edm::RefToBase<Base>> CVal;
+    typedef edm::AssociationMap<edm::OneToManyWithQuality<CKey, CVal, double, unsigned char> > Assoc;
+
+    CKey keys{1, 2, 3};
+    Data data{Derived(1.0), Derived(2.0), Derived(3.0), Derived(4.0)};
+
+    CVal values{
+      edm::RefToBase<Base>(edm::Ref<Data>(&data, 0)),
+      edm::RefToBase<Base>(edm::Ref<Data>(&data, 1)),
+      edm::RefToBase<Base>(edm::Ref<Data>(&data, 2))
+    };
+
+    Assoc map;
+    map.insert(edm::Ref<CKey>(&keys, 0), std::make_pair(edm::Ref<CVal>(&values, 0), 0.1));
+    map.insert(edm::Ref<CKey>(&keys, 1), std::make_pair(edm::Ref<CVal>(&values, 1), 0.2));
+    map.insert(edm::Ref<CKey>(&keys, 2), std::make_pair(edm::Ref<CVal>(&values, 1), 0.3));
+    map.insert(edm::Ref<CKey>(&keys, 2), std::make_pair(edm::Ref<CVal>(&values, 0), 0.4));
+    map.insert(edm::Ref<CKey>(&keys, 3), std::make_pair(edm::Ref<CVal>(&values, 2), 0.5));
+
+    std::vector<edm::RefToBase<Base>> keep{
+      edm::RefToBase<Base>(edm::Ref<Data>(&data, 1)),
+      edm::RefToBase<Base>(edm::Ref<Data>(&data, 2))
+    };
+
+    Assoc filtered = associationMapFilterValues(map, keep);
+    CPPUNIT_ASSERT( filtered.size() == 3 );
+    CPPUNIT_ASSERT( filtered.find(edm::Ref<CKey>(&keys, 0)) == filtered.end() );
+    auto found = filtered.find(edm::Ref<CKey>(&keys, 1));
+    CPPUNIT_ASSERT( found != filtered.end() );
+    CPPUNIT_ASSERT( found->val.size() == 1 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.2 );
+    found = filtered.find(edm::Ref<CKey>(&keys, 2));
+    CPPUNIT_ASSERT( found != filtered.end() );
+    CPPUNIT_ASSERT( found->val.size() == 1 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.3 );
+    found = filtered.find(edm::Ref<CKey>(&keys, 3));
+    CPPUNIT_ASSERT( found != filtered.end() );
+    CPPUNIT_ASSERT( found->val.size() == 1 );
+    CPPUNIT_ASSERT( found->val[0].second == 0.5 );
   }
 }
