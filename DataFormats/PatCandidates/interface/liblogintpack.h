410fb17f5b6c9223885fcb70440544a64a9d1e22
Matti Kortelainen <matti.kortelainen@cern.ch>
2015-10-21 14:58:24 +0200

    Fix liblogintpack for smallest negative value
    Before the smallest positive value was returned instead

--- DataFormats/PatCandidates/interface/liblogintpack.h
+++ DataFormats/PatCandidates/interface/liblogintpack.h
@@ -5,6 +5,9 @@
 
 namespace logintpack
 {
+        constexpr int8_t smallestPositive = 0;
+        constexpr int8_t smallestNegative = -128; // this value is free, so use it to denote the smallest (by absolute value) negative number
+
         int8_t pack8logCeil(double x,double lmin, double lmax, uint8_t base=128)
         {
                 if(base>128) base=128;
@@ -13,7 +16,10 @@ namespace logintpack
                 int8_t  r=ceil(centered);
                 if(centered >= base-1) r=base-1;
                 if(centered < 0) r=0;
-                if(x<0) r=-r;
+                if(x<0) {
+                  if(r==0) r=smallestNegative;
+                  else     r=-r;
+                }
                 return r;
         }
 
@@ -25,7 +31,10 @@ namespace logintpack
 		int8_t  r=centered;
 		if(centered >= base-1) r=base-1;
 		if(centered < 0) r=0;
-		if(x<0) r=-r;
+                if(x<0) {
+                  if(r==0) r=smallestNegative;
+                  else     r=-r;
+                }
 		return r;
 	}
 
@@ -39,7 +48,10 @@ namespace logintpack
 		int8_t  r=round(centered);
 		if(centered >= base-1) r=base-1;
 		if(centered < 0) r=0;
-		if(x<0) r=-r;
+                if(x<0) {
+                  if(r==0) r=smallestNegative;
+                  else     r=-r;
+                }
 		return r;
 	}
 
@@ -48,7 +60,9 @@ namespace logintpack
 	{
 	        if(base>128) base=128;
 	        float basef=base;
+                //int8_t num = i == smallestNegative ? 0 : std::abs(i);
 		float l=lmin+abs(i)/basef*(lmax-lmin);
+                if(i==smallestNegative) l = lmin;
 		float val=exp(l);
 		if(i<0) return -val; else return val;
 	}
@@ -60,6 +74,7 @@ namespace logintpack
 	        float basef=base-1;
 		float l=lmin+abs(i)/basef*(lmax-lmin);
                 if (abs(i) == base-1) l = lmax;
+                else if (i==smallestNegative) l = lmin;
 		float val=exp(l);
 		if(i<0) return -val; else return val;
 	}
