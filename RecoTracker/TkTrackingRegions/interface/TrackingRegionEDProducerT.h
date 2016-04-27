b98150218ae7c92a9b95e0ffea8dc28b2ee022b9
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-04-27 10:12:50 +0200

    Add dictionaries and move to OwnVector

--- RecoTracker/TkTrackingRegions/interface/TrackingRegionEDProducerT.h
+++ RecoTracker/TkTrackingRegions/interface/TrackingRegionEDProducerT.h
@@ -6,11 +6,15 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 #include "RecoTracker/TkTrackingRegions/interface/TrackingRegion.h"
+#include "DataFormats/Common/interface/OwnVector.h"
 
 template <typename T_TrackingRegionProducer>
 class TrackingRegionEDProducerT: public edm::stream::EDProducer<> {
 public:
-  using ProductType = std::vector<std::unique_ptr<TrackingRegion>>;
+  // using OwnVector as vector<shared_ptr> and vector<unique_ptr> cause problems
+  // I can't get dictionary compiled with unique_ptr
+  // shared_ptr fails with runtime error "Class name 'TrackingRegionstdshared_ptrs' contains an underscore ('_'), which is illegal in the name of a product."
+  using ProductType = edm::OwnVector<TrackingRegion>;
 
   TrackingRegionEDProducerT(const edm::ParameterSet& iConfig):
     regionProducer_(iConfig, consumesCollector()) {
@@ -24,8 +28,14 @@ public:
   }
 
   virtual void produce(edm::Event& iEvent, const edm::EventSetup& iSetup) override {
-    auto regions = std::make_unique<ProductType>(regionProducer_.regions(iEvent, iSetup));
-    iEvent.put(std::move(regions));
+    auto regions = regionProducer_.regions(iEvent, iSetup);
+    auto ret = std::make_unique<ProductType>();
+    ret->reserve(regions.size());
+    for(auto& regionPtr: regions) {
+      ret->push_back(regionPtr.release());
+    }
+
+    iEvent.put(std::move(ret));
   }
 
 private:
--- RecoTracker/TkTrackingRegions/src/classes.h
+++ RecoTracker/TkTrackingRegions/src/classes.h
@@ -1,8 +1,10 @@
 #include "RecoTracker/TkTrackingRegions/interface/TrackingRegion.h"
+#include "DataFormats/Common/interface/OwnVector.h"
 #include "DataFormats/Common/interface/Wrapper.h"
 
 namespace RecoTracker_TkTrackingRegions {
   struct dictionary {
-    edm::Wrapper<std::vector<std::unique_ptr<TrackingRegion> > > wvutr;
+    edm::OwnVector<TrackingRegion> ovtr;
+    edm::Wrapper<edm::OwnVector<TrackingRegion> > wovtr;
   };
 }
