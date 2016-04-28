c37a581f1ad22556ccfd8f7a7d5d1442283fb38d
Matti Kortelainen <matti.kortelainen@cern.ch>
2016-04-28 17:15:16 +0200

    Cleanup

--- RecoTracker/TkHitPairs/interface/IntermediateHitDoublets.h
+++ RecoTracker/TkHitPairs/interface/IntermediateHitDoublets.h
@@ -12,12 +12,6 @@ class IntermediateHitDoublets {
 public:
   using LayerPair = std::tuple<SeedingLayerSetsHits::LayerIndex, SeedingLayerSetsHits::LayerIndex>;
 
-  /*
-  IntermediateHitDoublets(HitDoublets&& doublets, LayerHitMapCache&& cache, const TrackingRegion *region):
-    doublets_(std::move(doublets)), cache_(std::move(cache)), region_(region)
-  {}
-  IntermediateHitDoublets(); // onty to make ROOT dictionary generation happy
-  */
   IntermediateHitDoublets(): seedingLayers_(nullptr) {}
   explicit IntermediateHitDoublets(const SeedingLayerSetsHits *seedingLayers): seedingLayers_(seedingLayers) {}
   IntermediateHitDoublets(const IntermediateHitDoublets& rh); // only to make ROOT dictionary generation happy
@@ -31,13 +25,6 @@ public:
   void reserve(size_t nregions, size_t nlayersets) {
     regions_.reserve(nregions);
     layerPairs_.reserve(nregions*nlayersets);
-#ifdef FOO
-    regions_->reserve(nregions);
-    layerPairIndices_->reserve(nregions);
-    layerPairs_->reserve(nregiogns*nlayersets);
-    doublets_->reserve(nregiogns*nlayersets);
-    caches_->reserve(nregiogns*nlayersets);
-#endif
   }
 
   void shrink_to_fit() {
@@ -47,26 +34,11 @@ public:
 
   void beginRegion(const TrackingRegion *region) {
     regions_.emplace_back(region, layerPairs_.size());
-#ifdef FOO
-    regions_.push_back(region);
-    layerPairIndices.push_back(layerPairs_.size());
-#endif
   }
 
   void addDoublets(const SeedingLayerSetsHits::SeedingLayerSet& layerSet, HitDoublets&& doublets, LayerHitMapCache&& cache) {
     layerPairs_.emplace_back(layerSet, std::move(doublets), std::move(cache));
-#ifdef FOO
-    layerPairs_.emplace_back(layerSet[0].index(), layerSet[1].index());
-    doublets_.emplace_back(std::move(doublets));
-    caches_.emplace_back(std::move(cache));
-#endif
   }
-
-  /*
-  const HitDoublets& hitDoublets() const { return doublets_; }
-  const LayerHitMapCache& hitCache() const { return cache_; }
-  const TrackingRegion& region() const { return *region_; }
-  */
 
 private:
   const SeedingLayerSetsHits *seedingLayers_;
@@ -92,34 +64,6 @@ private:
 
   std::vector<RegionIndex> regions_;
   std::vector<LayerPairHitDoublets> layerPairs_;
-
-#ifdef FOO
-
-  /**
-   * TrackingRegions
-   */
-  std::vector<const TrackingRegion *> regions_;
-
-  /**
-   * Has same size as regions_. Each element is an index to
-   * layerPairs_, doublets_, and caches_ and points to the beginning
-   * of those for that TrackingRegion
-   */
-  std::vector<unsigned int> layerPairIndices;
-
-  /**
-   * Identifies the seeding layer pair used for hit doublets
-   */ 
-  std::vector<LayerPair> layerPairs_;
-  std::vector<HitDoublets> doublets_;
-  std::vector<LayerHitMapCache> caches_;
-#endif
-
-  /*
-  HitDoublets doublets_;
-  LayerHitMapCache cache_;
-  const TrackingRegion *region_;
-  */
 };
 
 #endif
--- RecoTracker/TkHitPairs/src/IntermediateHitDoublets.cc
+++ RecoTracker/TkHitPairs/src/IntermediateHitDoublets.cc
@@ -1,11 +1,6 @@
 #include "RecoTracker/TkHitPairs/interface/IntermediateHitDoublets.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
-/*
-IntermediateHitDoublets::IntermediateHitDoublets() {
-  throw cms::Exception("LogicError") << "The default constructor of IntermediateHitDoublets should never be called. The function exists only to make ROOT dictionary generation happy";
-}
-*/
 IntermediateHitDoublets::IntermediateHitDoublets(const IntermediateHitDoublets& rh) {
   throw cms::Exception("Not Implemented") << "The copy constructor of IntermediateHitDoublets should never be called. The function exists only to make ROOT dictionary generation happy";
 }
