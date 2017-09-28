cd7bfc194224da1eb7475e5151bcedc27aa70998
erica brondolin <erica.brondolin@cern.ch>
2017-09-28 16:06:08 +0200

    fix indentation

--- SimTracker/TrackAssociatorProducers/plugins/QuickTrackAssociatorByHitsImpl.cc
+++ SimTracker/TrackAssociatorProducers/plugins/QuickTrackAssociatorByHitsImpl.cc
@@ -411,66 +411,60 @@ template<typename iter> std::vector<OmniClusterRef> QuickTrackAssociatorByHitsIm
 
       if (typeid(*rhit) == typeid(SiTrackerMultiRecHit)) {
         LogDebug("TrackAssociator") << "multi rechit associated!"; 
-	const SiTrackerMultiRecHit* mHit = dynamic_cast<const SiTrackerMultiRecHit*>(rhit);
+        const SiTrackerMultiRecHit* mHit = dynamic_cast<const SiTrackerMultiRecHit*>(rhit);
         rhit = getMostProbableHit(mHit);
       }
 
       int subdetid = rhit->geographicalId().subdetId();
       if (subdetid==PixelSubdetector::PixelBarrel||subdetid==PixelSubdetector::PixelEndcap) {
 
-	const std::type_info &tid = typeid(*rhit);
+        const std::type_info &tid = typeid(*rhit);
         LogDebug("TrackAssociator") << "rhit type " << typeid(*rhit).name();
-	if (tid == typeid(SiPixelRecHit)) {
-	  const SiPixelRecHit* pRHit = dynamic_cast<const SiPixelRecHit*>(rhit);
-	  if (!pRHit->cluster().isNonnull())
-	    edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
-	  returnValue.push_back(pRHit->omniClusterRef());
-        }
-	else {
-	  edm::LogError("TrackAssociator") << ">>> getMatchedClusters: TrackingRecHit not associated to any SiPixelCluster! subdetid = " << subdetid;
+        if (tid == typeid(SiPixelRecHit)) {
+          const SiPixelRecHit* pRHit = dynamic_cast<const SiPixelRecHit*>(rhit);
+          if (!pRHit->cluster().isNonnull())
+            edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
+          returnValue.push_back(pRHit->omniClusterRef());
+        } else {
+          edm::LogError("TrackAssociator") << ">>> getMatchedClusters: TrackingRecHit not associated to any SiPixelCluster! subdetid = " << subdetid;
         }
 
       }
       else if (subdetid==SiStripDetId::TIB||subdetid==SiStripDetId::TOB||subdetid==SiStripDetId::TID||subdetid==SiStripDetId::TEC) {
 
-	const std::type_info &tid = typeid(*rhit);
+        const std::type_info &tid = typeid(*rhit);
         LogDebug("TrackAssociator") << "rhit type " << typeid(*rhit).name();
-
-	if (tid == typeid(SiStripMatchedRecHit2D)) {
-	  const SiStripMatchedRecHit2D* sMatchedRHit = dynamic_cast<const SiStripMatchedRecHit2D*>(rhit);
-	  if (!sMatchedRHit->monoHit().cluster().isNonnull() || !sMatchedRHit->stereoHit().cluster().isNonnull())
-	    edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
-	  returnValue.push_back(sMatchedRHit->monoClusterRef());
-	  returnValue.push_back(sMatchedRHit->stereoClusterRef());
-	}
-	else if (tid == typeid(SiStripRecHit2D)) {
-	  const SiStripRecHit2D* sRHit = dynamic_cast<const SiStripRecHit2D*>(rhit);
-	  if (!sRHit->cluster().isNonnull())
-	    edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
-	  returnValue.push_back(sRHit->omniClusterRef());
-	}
-	else if (tid == typeid(SiStripRecHit1D)) {
-	  const SiStripRecHit1D* sRHit = dynamic_cast<const SiStripRecHit1D*>(rhit);
-	  if (!sRHit->cluster().isNonnull())
-	    edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
-	  returnValue.push_back(sRHit->omniClusterRef());
-	}
-	else if (tid == typeid(Phase2TrackerRecHit1D)) {
-	  const Phase2TrackerRecHit1D* ph2Hit = dynamic_cast<const Phase2TrackerRecHit1D*>(rhit);
+        
+        if (tid == typeid(SiStripMatchedRecHit2D)) {
+          const SiStripMatchedRecHit2D* sMatchedRHit = dynamic_cast<const SiStripMatchedRecHit2D*>(rhit);
+          if (!sMatchedRHit->monoHit().cluster().isNonnull() || !sMatchedRHit->stereoHit().cluster().isNonnull())
+            edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
+          returnValue.push_back(sMatchedRHit->monoClusterRef());
+          returnValue.push_back(sMatchedRHit->stereoClusterRef());
+        } else if (tid == typeid(SiStripRecHit2D)) {
+          const SiStripRecHit2D* sRHit = dynamic_cast<const SiStripRecHit2D*>(rhit);
+          if (!sRHit->cluster().isNonnull())
+            edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
+          returnValue.push_back(sRHit->omniClusterRef());
+        } else if (tid == typeid(SiStripRecHit1D)) {
+          const SiStripRecHit1D* sRHit = dynamic_cast<const SiStripRecHit1D*>(rhit);
+          if (!sRHit->cluster().isNonnull())
+            edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
+          returnValue.push_back(sRHit->omniClusterRef());
+        } else if (tid == typeid(Phase2TrackerRecHit1D)) {
+          const Phase2TrackerRecHit1D* ph2Hit = dynamic_cast<const Phase2TrackerRecHit1D*>(rhit);
           if (!ph2Hit->cluster().isNonnull() )
-	    edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
-	  returnValue.push_back(ph2Hit->omniClusterRef());
+            edm::LogError("TrackAssociator") << ">>> RecHit does not have an associated cluster!" << " file: " << __FILE__ << " line: " << __LINE__;
+          returnValue.push_back(ph2Hit->omniClusterRef());
+        } else {
+          auto const & thit = static_cast<BaseTrackerRecHit const&>(*rhit);
+          if ( thit.isProjected() ) {
+          } else {
+            edm::LogError("TrackAssociator") << ">>> getMatchedClusters: TrackingRecHit not associated to any SiStripCluster! subdetid = " << subdetid;
+          }
         }
-	else {
-	  auto const & thit = static_cast<BaseTrackerRecHit const&>(*rhit);
-	  if ( thit.isProjected() ) {
-	  } else {
-	    edm::LogError("TrackAssociator") << ">>> getMatchedClusters: TrackingRecHit not associated to any SiStripCluster! subdetid = " << subdetid;
-	  }
-	}
-      }
-      else {
-	edm::LogError("TrackAssociator") << ">>> getMatchedClusters: TrackingRecHit not associated to any cluster! subdetid = " << subdetid;
+      } else {
+        edm::LogError("TrackAssociator") << ">>> getMatchedClusters: TrackingRecHit not associated to any cluster! subdetid = " << subdetid;
       }
     }
   }
