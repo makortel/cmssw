3bc39f180bc29926b056e5f32e0f8e496855e340
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-10-18 14:06:41 +0200

    Generalize the interface of the EDProducer, next have to figure out how to communicate with the generator

--- RecoPixelVertexing/PixelTriplets/plugins/CAHitNtupletEDProducer.cc
+++ RecoPixelVertexing/PixelTriplets/plugins/CAHitNtupletEDProducer.cc
@@ -35,6 +35,7 @@ public:
 
 private:
   edm::EDGetTokenT<IntermediateHitDoublets> doubletToken_;
+  std::vector<unsigned int> forNLayers_;
 
   edm::RunningAverage localRA_;
 
@@ -43,15 +44,28 @@ private:
 
 CAHitNtupletEDProducer::CAHitNtupletEDProducer(const edm::ParameterSet& iConfig):
   doubletToken_(consumes<IntermediateHitDoublets>(iConfig.getParameter<edm::InputTag>("doublets"))),
-  generator_(iConfig, consumesCollector())
+  forNLayers_(iConfig.getParameter<std::vector<unsigned int> >("forNLayers")),
+  generator_(iConfig, consumesCollector()) // TODO: how to deliver the "number of layers" to the generator?
 {
-  produces<RegionsSeedingHitSets>();
+  // Sanity checks
+  if(forNLayers_.empty()) throw cms::Exception("Configuration") << "forNLayers is empty, need at least one element";
+  unsigned prev = forNLayers_[0];
+  for(size_t i=1, end=forNLayers_.size(); i<end; ++i) {
+    if(forNLayers_[i] <= prev) throw cms::Exception("Configuration") << "Elements of forNLayers must be in ascending order, now element " << i << " is " << forNLayers_[i] << " while the element " << (i-1) << " was " << prev;
+    prev = forNLayers_[i];
+  }
+
+  // Setup output
+  for(auto nlayers: forNLayers_) {
+    produces<RegionsSeedingHitSets>(std::to_string(nlayers));
+  }
 }
 
 void CAHitNtupletEDProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
 
   desc.add<edm::InputTag>("doublets", edm::InputTag("hitPairEDProducer"));
+  desc.add<std::vector<unsigned int>>("forNLayers", std::vector<unsigned int>{{4}})->setComment("Produce separate collections of ntuplets for each \"number of layers\", i.e. triplets for 3, quadruplets for 4 etc. The last number is interpreted as \"ntuplets from at least this many layers\"."); // by default quadruplets only
   CAHitNtupletGenerator::fillDescriptions(desc);
 
   auto label = CAHitNtupletGenerator::fillDescriptionsLabel() + std::string("EDProducer");
@@ -68,12 +82,19 @@ void CAHitNtupletEDProducer::produce(edm::Event& iEvent, const edm::EventSetup& 
     throw cms::Exception("LogicError") << "CAHitNtupletEDProducer expects SeedingLayerSetsHits::numberOfLayersInSet() to be >= " << CAHitNtupletGenerator::minLayers << ", got " << seedingLayerHits.numberOfLayersInSet() << ". This is likely caused by a configuration error of this module, HitPairEDProducer, or SeedingLayersEDProducer.";
   }
 
-  auto seedingHitSets = std::make_unique<RegionsSeedingHitSets>();
+  std::vector<std::unique_ptr<RegionsSeedingHitSets> > seedingHitSets(forNLayers_.size());
+  for(size_t i=0, end=forNLayers_.size(); i<end; ++i) {
+    seedingHitSets[i] = std::make_unique<RegionsSeedingHitSets>();
+  }
   if(regionDoublets.empty()) {
-    iEvent.put(std::move(seedingHitSets));
+    for(size_t i=0, end=forNLayers_.size(); i<end; ++i) {
+      iEvent.put(std::move(seedingHitSets[i]), std::to_string(forNLayers_[i]));
+    }
     return;
   }
-  seedingHitSets->reserve(regionDoublets.regionSize(), localRA_.upper());
+  for(size_t i=0, end=forNLayers_.size(); i<end; ++i) {
+    seedingHitSets[i]->reserve(regionDoublets.regionSize(), localRA_.upper());
+  }
   generator_.initEvent(iEvent, iSetup);
 
   LogDebug("CAHitNtupletEDProducer") << "Creating ntuplets for " << regionDoublets.regionSize() << " regions, and " << regionDoublets.layerPairsSize() << " layer pairs";
@@ -81,19 +102,23 @@ void CAHitNtupletEDProducer::produce(edm::Event& iEvent, const edm::EventSetup& 
   ntuplets.resize(regionDoublets.regionSize());
   for(auto& ntuplet : ntuplets)  ntuplet.reserve(localRA_.upper());
 
-  generator_.hitNtuplets(regionDoublets, ntuplets, iSetup, seedingLayerHits);
+  generator_.hitNtuplets(regionDoublets, ntuplets, iSetup, seedingLayerHits); // TODO: how exactly to communicate with the generator about the different layer-number ntuplets?
   int index = 0;
   for(const auto& regionLayerPairs: regionDoublets) {
     const TrackingRegion& region = regionLayerPairs.region();
-    auto seedingHitSetsFiller = seedingHitSets->beginRegion(&region);
+    auto seedingHitSetsFiller = seedingHitSets[0]->beginRegion(&region); // TODO: eventually must have a loop over seedingHitSets
 
-    fillNtuplets(seedingHitSetsFiller, ntuplets[index]);
+    fillNtuplets(seedingHitSetsFiller, ntuplets[index]); // TODO: need to generalize this as well
     ntuplets[index].clear();
     index++;
   }
-  localRA_.update(seedingHitSets->size());
 
-  iEvent.put(std::move(seedingHitSets));
+  size_t maxSize=0;
+  for(size_t i=0, end=forNLayers_.size(); i<end; ++i) {
+    maxSize = std::max(maxSize, seedingHitSets[i]->size());
+    iEvent.put(std::move(seedingHitSets[i]), std::to_string(forNLayers_[i]));
+  }
+  localRA_.update(maxSize);
 }
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
