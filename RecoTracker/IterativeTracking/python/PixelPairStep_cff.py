96484b021a6fb2015e99112afdc0c7ad661cd092
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-11-08 16:34:12 +0100

    Ignore single inactive panels

--- RecoTracker/IterativeTracking/python/PixelPairStep_cff.py
+++ RecoTracker/IterativeTracking/python/PixelPairStep_cff.py
@@ -156,6 +156,7 @@ pixelPairStepTrackingRegionsSeedLayersB = _pixelInactiveAreaTrackingRegionsAndSe
         zErrorVertex = 0.03,
         maxNVertices = 5,
     ),
+    ignoreSingleFPixPanelModules = True,
 #    debug = True,
 )
 pixelPairStepHitDoubletsB = pixelPairStepHitDoublets.clone(
