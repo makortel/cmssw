4a43b47cc26a6fda4c6bb21e3d65290daad22114
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-08 14:51:14 +0100

    Migrate EleIdCutBasedRef,EleIdNeuralNetRef,EleIdLikelihoodRef,EleIdCutBased

--- RecoEgamma/ElectronIdentification/plugins/SealModule.cc
+++ RecoEgamma/ElectronIdentification/plugins/SealModule.cc
@@ -2,6 +2,8 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/ESProducer.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 
@@ -28,6 +30,35 @@ typedef ObjectSelector<
           EleIdLikelihoodSel, 
           edm::RefVector<reco::GsfElectronCollection> 
          > EleIdLikelihoodRef ;
+template<>
+void EleIdCutBasedRef::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  // With the current structure it is impossible to create a fully
+  // working fillDescriptions, so treat as if fillDescriptions would
+  // not be implemented.
+  ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+template<>
+void EleIdNeuralNetRef::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  // The configuration would be simple, but it would be impossible to
+  // continue with the existing naming convention where the module
+  // labels (eid*) and cfi files (electronId*) differ. The real
+  // implementation is left for the domain experts.
+  ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+template<>
+void EleIdLikelihoodRef::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  // The configuration would be simple, but it would be impossible to
+  // continue with the existing naming convention where the module
+  // labels (eid*) and cfi files (electronId*) differ. The real
+  // implementation is left for the domain experts.
+  ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
 DEFINE_FWK_MODULE(EleIdCutBasedRef);
 DEFINE_FWK_MODULE(EleIdNeuralNetRef);
 DEFINE_FWK_MODULE(EleIdLikelihoodRef);
@@ -45,4 +76,13 @@ DEFINE_FWK_MODULE(EleIdLikelihoodExtProducer);
 DEFINE_FWK_EVENTSETUP_MODULE( ElectronLikelihoodESSource );
 
 typedef ObjectSelector<EleIdCutBasedSel, reco::GsfElectronCollection> EleIdCutBased;
+template<>
+void EleIdCutBased::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  // With the current structure it is impossible to create a fully
+  // working fillDescriptions, so treat as if fillDescriptions would
+  // not be implemented.
+  ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
 DEFINE_FWK_MODULE(EleIdCutBased);
