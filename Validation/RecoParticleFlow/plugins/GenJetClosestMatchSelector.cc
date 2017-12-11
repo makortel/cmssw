61f3e6eea9e1338e07a184a63c62bdd739a4fb74
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-11 13:31:02 +0100

    Migrate GenJetClosestMatchSelector

--- Validation/RecoParticleFlow/plugins/GenJetClosestMatchSelector.cc
+++ Validation/RecoParticleFlow/plugins/GenJetClosestMatchSelector.cc
@@ -1,10 +1,20 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
 #include "CommonTools/UtilAlgos/interface/ObjectSelector.h"
 #include "Validation/RecoParticleFlow/plugins/GenJetClosestMatchSelectorDefinition.h"
 
 typedef ObjectSelector<GenJetClosestMatchSelectorDefinition> GenJetClosestMatchSelector;
+template<>
+void GenJetClosestMatchSelector::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<edm::InputTag>("src", edm::InputTag("ak4GenJets"));
+  desc.add<edm::InputTag>("MatchTo", edm::InputTag("tauGenJetsSelectorAllHadrons"));
+  desc.add<bool>("filter", false);
+  descriptions.addWithDefaultLabel(desc);
+}
 
 DEFINE_FWK_MODULE(GenJetClosestMatchSelector);
 
