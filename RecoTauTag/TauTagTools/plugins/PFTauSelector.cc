0cf4825b6b4efbe9f49de6432491c4e047adbd2d
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-11 11:26:46 +0100

    Migrate PFTauSelector

--- RecoTauTag/TauTagTools/plugins/PFTauSelector.cc
+++ RecoTauTag/TauTagTools/plugins/PFTauSelector.cc
@@ -1,9 +1,27 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 
 #include "CommonTools/UtilAlgos/interface/ObjectSelectorStream.h"
 #include "RecoTauTag/TauTagTools/plugins/PFTauSelectorDefinition.h"
 
 typedef ObjectSelectorStream<PFTauSelectorDefinition> PFTauSelector;
+template<>
+void PFTauSelector::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.add<edm::InputTag>("src", edm::InputTag("fixedConePFTauProducer"));
+  desc.add<std::string>("cut", "pt > 0");
+
+  edm::ParameterSetDescription validator;
+  validator.add<edm::InputTag>("discriminator", edm::InputTag());
+  validator.add<double>("selectionCut", 0.);
+  edm::ParameterSet defaults;
+  defaults.addParameter<edm::InputTag>("descriminator", edm::InputTag("fixedConePFTauDiscriminationByIsolation"));
+  defaults.addParameter<double>("selectionCut", 0.5);
+  desc.addVPSet("discriminators", validator, std::vector<edm::ParameterSet>{defaults});
+
+  descriptions.addWithDefaultLabel(desc);
+}
 
 DEFINE_FWK_MODULE(PFTauSelector);
