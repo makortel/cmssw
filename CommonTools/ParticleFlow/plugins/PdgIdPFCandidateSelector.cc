77fd631f8e57a3fe708bb9a7d189872c39ababac
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-12 17:17:49 +0100

    Migrate PdgIdPFCandidateSelector
    The cfi file is not used anywhere so I try to remove it.

--- CommonTools/ParticleFlow/plugins/PdgIdPFCandidateSelector.cc
+++ CommonTools/ParticleFlow/plugins/PdgIdPFCandidateSelector.cc
@@ -1,5 +1,7 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
 
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
@@ -9,5 +11,13 @@
 #include "CommonTools/ParticleFlow/interface/PdgIdPFCandidateSelectorDefinition.h"
 
 typedef ObjectSelector<pf2pat::PdgIdPFCandidateSelectorDefinition> PdgIdPFCandidateSelector;
+
+template<>
+void PdgIdPFCandidateSelector::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  fillPSetSrc(desc, edm::InputTag("pfNoPileUpIso"));
+  desc.add<std::vector<int> >("pdgId", std::vector<int>{22,111,130,310,2112});
+  descriptions.addWithDefaultLabel(desc);
+}
 
 DEFINE_FWK_MODULE(PdgIdPFCandidateSelector);
