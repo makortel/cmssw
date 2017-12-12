eabb4a08fe57d2d614ff114d32c43d8235c44dcc
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-12 17:52:30 +0100

    Migrate GenericPFJetSlector

--- CommonTools/ParticleFlow/interface/GenericPFJetSelectorDefinition.h
+++ CommonTools/ParticleFlow/interface/GenericPFJetSelectorDefinition.h
@@ -5,6 +5,7 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "DataFormats/JetReco/interface/PFJet.h"
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
 #include "CommonTools/ParticleFlow/interface/PFJetSelectorDefinition.h"
@@ -16,6 +17,10 @@ namespace pf2pat {
 
     GenericPFJetSelectorDefinition ( const edm::ParameterSet & cfg, edm::ConsumesCollector && iC ) :
       selector_( cfg.getParameter< std::string >( "cut" ) ) { }
+
+    static void fillPSetDescription(edm::ParameterSetDescription& desc) {
+      desc.add<std::string>("cut", "");
+    }
 
     void select( const HandleToCollection & hc,
 		 const edm::Event & e,
