da5a8c218b2d2f9d22d4b85f7f877f25b8e89386
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-15 12:57:13 +0100

    Migrate MaxSelector, PairSelector, RefSelector

--- CommonTools/UtilAlgos/interface/MaxSelector.h
+++ CommonTools/UtilAlgos/interface/MaxSelector.h
@@ -1,6 +1,7 @@
 #ifndef UtilAlgos_MaxSelector_h
 #define UtilAlgos_MaxSelector_h
 #include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "CommonTools/UtilAlgos/interface/ParameterAdapter.h"
 #include "CommonTools/Utils/interface/MaxSelector.h"
 
@@ -11,6 +12,10 @@ namespace reco {
     struct ParameterAdapter<MaxSelector<T> > {
       static MaxSelector<T> make( const edm::ParameterSet & cfg, edm::ConsumesCollector & iC ) {
 	return MaxSelector<T>( cfg.template getParameter<double>( "max" ) );
+      }
+
+      static void fillPSetDescription(edm::ParameterSetDescription& desc) {
+        desc.add<double>("max", 0.);
       }
     };
 
--- CommonTools/UtilAlgos/interface/RefSelector.h
+++ CommonTools/UtilAlgos/interface/RefSelector.h
@@ -1,6 +1,7 @@
 #ifndef UtilAlgos_RefSelector_h
 #define UtilAlgos_RefSelector_h
 #include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/ParameterSet/interface/ParameterSetfwd.h"
 #include "CommonTools/UtilAlgos/interface/ParameterAdapter.h"
 #include "CommonTools/Utils/interface/RefSelector.h"
 
@@ -11,6 +12,9 @@ namespace reco {
     struct ParameterAdapter<RefSelector<S> > {
       static RefSelector<S> make( const edm::ParameterSet & cfg, edm::ConsumesCollector & iC ) {
 	return RefSelector<S>( modules::make<S>( cfg, iC ) );
+      }
+      static void fillPSetDescription(edm::ParameterSetDescription& desc) {
+        modules::fillPSetDescription<S>(desc);
       }
     };
 
