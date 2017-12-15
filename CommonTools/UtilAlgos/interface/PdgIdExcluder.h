ee8fa4812720a9962e44727e8db85d1c43597288
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-15 13:29:05 +0100

    Migrate PdgIdExcluder

--- CommonTools/UtilAlgos/interface/PdgIdExcluder.h
+++ CommonTools/UtilAlgos/interface/PdgIdExcluder.h
@@ -1,6 +1,7 @@
 #ifndef CommonTools_UtilAlgos_PdgIdSelector_h
 #define CommonTools_UtilAlgos_PdgIdSelector_h
 #include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 #include "CommonTools/UtilAlgos/interface/ParameterAdapter.h"
 #include "CommonTools/Utils/interface/PdgIdExcluder.h"
 
@@ -11,6 +12,10 @@ namespace reco {
       struct ParameterAdapter<PdgIdExcluder> {
 	static PdgIdExcluder make( const edm::ParameterSet & cfg, edm::ConsumesCollector & iC ) {
 	  return PdgIdExcluder( cfg.getParameter<std::vector<int> >( "pdgId" ) );
+	}
+
+	static void fillPSetDescription(edm::ParameterSetDescription& desc) {
+	  desc.add<std::vector<std::string> >("pdgId", std::vector<std::string>{});
 	}
       };
 
