2113b28b22a9599373dd047bbb86108302875fef
Matti Kortelainen <matti.kortelainen@cern.ch>
2017-12-28 19:46:28 +0100

    Migrate GenParticleCustomSelector

--- CommonTools/CandAlgos/plugins/GenParticleCustomSelector.cc
+++ CommonTools/CandAlgos/plugins/GenParticleCustomSelector.cc
@@ -7,6 +7,8 @@
  */
 
 #include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
 #include "CommonTools/UtilAlgos/interface/SingleObjectSelector.h"
 #include "CommonTools/CandAlgos/interface/GenParticleCustomSelector.h"
 
@@ -14,7 +16,24 @@ namespace reco {
   namespace modules {
     typedef SingleObjectSelector<GenParticleCollection,::GenParticleCustomSelector> 
     GenParticleCustomSelector ;
+  }
+}
+template <>
+reco::modules::GenParticleCustomSelector::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  fillPSetSrc(desc, edm::InputTag("genParticles"));
+  desc.add<bool>("chargedOnly", true);
+  desc.add<int>("status", 1);
+  desc.add<std::vector<int> >("pdgId", std::vector<int>{});
+  desc.add<double>("tip", 3.5);
+  desc.add<double>("lip", 30.);
+  desc.add<double>("ptMin", 0.9);
+  desc.add<double>("minRapidity", -2.4);
+  desc.add<double>("maxRapidity", 2.4);
+}
 
+namespace reco {
+  namespace modules {
     DEFINE_FWK_MODULE( GenParticleCustomSelector );
   }
 }
