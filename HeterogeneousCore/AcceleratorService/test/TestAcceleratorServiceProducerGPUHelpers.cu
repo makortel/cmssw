265b99740411e27f407740931b600443a5f6219b
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-01-24 15:12:14 +0100

    Migrate to cuda-api-wrappers

--- HeterogeneousCore/AcceleratorService/test/TestAcceleratorServiceProducerGPUHelpers.cu
+++ HeterogeneousCore/AcceleratorService/test/TestAcceleratorServiceProducerGPUHelpers.cu
@@ -1,5 +1,6 @@
 #include "TestAcceleratorServiceProducerGPUHelpers.h"
 
+#include <cuda/api_wrappers.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
@@ -9,47 +10,48 @@
 namespace {
 template<typename T>
 __global__
-void vectorAdd(T *a, T *b, T *c) {
+void vectorAdd(const T *a, const T *b, T *c, int numElements) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    c[i] = a[i] + b[i];
+    if(i < numElements) { c[i] = a[i] + b[i]; }
 }
 }
 
 int TestAcceleratorServiceProducerGPUHelpers_simple_kernel(int input) {
-  // Example from Viktor
+  // Example from Viktor/cuda-api-wrappers
   constexpr int NUM_VALUES = 10000;
 
-  cudaStream_t stream;
+  auto current_device = cuda::device::current::get();
+  auto stream = current_device.create_stream(cuda::stream::implicitly_synchronizes_with_default_stream);
 
-  cudaStreamCreate(&stream);
-  
-  int h_a[NUM_VALUES], h_b[NUM_VALUES], h_c[NUM_VALUES];
+  auto h_a = std::make_unique<int[]>(NUM_VALUES);
+  auto h_b = std::make_unique<int[]>(NUM_VALUES);
+  auto h_c = std::make_unique<int[]>(NUM_VALUES);
+
   for (auto i=0; i<NUM_VALUES; i++) {
     h_a[i] = input + i;
     h_b[i] = i*i;
   }
 
-  int *d_a, *d_b, *d_c;
-  cudaMalloc(&d_a, NUM_VALUES*sizeof(int));
-  cudaMalloc(&d_b, NUM_VALUES*sizeof(int));
-  cudaMalloc(&d_c, NUM_VALUES*sizeof(int));
+  auto d_a = cuda::memory::device::make_unique<int[]>(current_device, NUM_VALUES);
+  auto d_b = cuda::memory::device::make_unique<int[]>(current_device, NUM_VALUES);
+  auto d_c = cuda::memory::device::make_unique<int[]>(current_device, NUM_VALUES);
 
-  cudaMemcpyAsync(d_a, h_a, NUM_VALUES*sizeof(int), cudaMemcpyHostToDevice, stream);
-  cudaMemcpyAsync(d_b, h_b, NUM_VALUES*sizeof(int), cudaMemcpyHostToDevice, stream);
+  cuda::memory::async::copy(d_a.get(), h_a.get(), NUM_VALUES*sizeof(int), stream.id());
+  cuda::memory::async::copy(d_b.get(), h_b.get(), NUM_VALUES*sizeof(int), stream.id());
 
   int threadsPerBlock {256};
   int blocksPerGrid = (NUM_VALUES + threadsPerBlock - 1) / threadsPerBlock;
-  vectorAdd<<<blocksPerGrid, threadsPerBlock, 0, stream>>>(d_a, d_b, d_c);
 
-  cudaMemcpyAsync(h_c, d_c, NUM_VALUES*sizeof(int), cudaMemcpyDeviceToHost, stream);
+  vectorAdd<<<blocksPerGrid, threadsPerBlock, 0, stream.id()>>>(d_a.get(), d_b.get(), d_c.get(), NUM_VALUES);
+  /*
+    // doesn't work with header-only?
+  cuda::launch(vectorAdd, {blocksPerGrid, threadsPerBlock},
+               d_a.get(), d_b.get(), d_c.get(), NUM_VALUES);
+  */
 
-  cudaStreamSynchronize(stream);
+  cuda::memory::async::copy(h_c.get(), d_c.get(), NUM_VALUES*sizeof(int), stream.id());
 
-  cudaFree(d_a);
-  cudaFree(d_b);
-  cudaFree(d_c);
-
-  cudaStreamDestroy(stream);
+  stream.synchronize();
 
   int ret = 0;
   for (auto i=0; i<10; i++) {
