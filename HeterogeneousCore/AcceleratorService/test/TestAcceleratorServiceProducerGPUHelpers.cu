a935ed174afc73833f7b32f8bd07839cf6f2d1ff
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-01-24 12:21:49 +0100

    First step towards asynchronous test

--- HeterogeneousCore/AcceleratorService/test/TestAcceleratorServiceProducerGPUHelpers.cu
+++ HeterogeneousCore/AcceleratorService/test/TestAcceleratorServiceProducerGPUHelpers.cu
@@ -1,5 +1,8 @@
 #include "TestAcceleratorServiceProducerGPUHelpers.h"
 
+#include <cuda.h>
+#include <cuda_runtime.h>
+
 //
 // Vector Addition Kernel
 //
@@ -15,6 +18,10 @@ void vectorAdd(T *a, T *b, T *c) {
 int TestAcceleratorServiceProducerGPUHelpers_simple_kernel(int input) {
   // Example from Viktor
   constexpr int NUM_VALUES = 10000;
+
+  cudaStream_t stream;
+
+  cudaStreamCreate(&stream);
   
   int h_a[NUM_VALUES], h_b[NUM_VALUES], h_c[NUM_VALUES];
   for (auto i=0; i<NUM_VALUES; i++) {
@@ -27,18 +34,22 @@ int TestAcceleratorServiceProducerGPUHelpers_simple_kernel(int input) {
   cudaMalloc(&d_b, NUM_VALUES*sizeof(int));
   cudaMalloc(&d_c, NUM_VALUES*sizeof(int));
 
-  cudaMemcpy(d_a, h_a, NUM_VALUES*sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_b, h_b, NUM_VALUES*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpyAsync(d_a, h_a, NUM_VALUES*sizeof(int), cudaMemcpyHostToDevice, stream);
+  cudaMemcpyAsync(d_b, h_b, NUM_VALUES*sizeof(int), cudaMemcpyHostToDevice, stream);
 
   int threadsPerBlock {256};
   int blocksPerGrid = (NUM_VALUES + threadsPerBlock - 1) / threadsPerBlock;
-  vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_a, d_b, d_c);
+  vectorAdd<<<blocksPerGrid, threadsPerBlock, 0, stream>>>(d_a, d_b, d_c);
 
-  cudaMemcpy(h_c, d_c, NUM_VALUES*sizeof(int), cudaMemcpyDeviceToHost);
+  cudaMemcpyAsync(h_c, d_c, NUM_VALUES*sizeof(int), cudaMemcpyDeviceToHost, stream);
+
+  cudaStreamSynchronize(stream);
 
   cudaFree(d_a);
   cudaFree(d_b);
   cudaFree(d_c);
+
+  cudaStreamDestroy(stream);
 
   int ret = 0;
   for (auto i=0; i<10; i++) {
