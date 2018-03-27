40fb3f70ed606346f530eb62e90c9f0aa17f0b45
Angira Rastogi <rastogiangira0908@gmail.com>
2018-03-27 16:33:22 +0200

    New phase1 iterations in FastSim

--- FastSimulation/Tracking/python/iterativeTk_cff.py
+++ FastSimulation/Tracking/python/iterativeTk_cff.py
@@ -6,6 +6,9 @@
 import FWCore.ParameterSet.Config as cms
 from TrackingTools.MaterialEffects.MaterialPropagatorParabolicMf_cff import *
 from FastSimulation.Tracking.InitialStep_cff import *
+from FastSimulation.Tracking.DetachedQuadStep_cff import *
+from FastSimulation.Tracking.HighPtTripletStep_cff import *
+from FastSimulation.Tracking.LowPtQuadStep_cff import *
 from FastSimulation.Tracking.DetachedTripletStep_cff import *
 from FastSimulation.Tracking.LowPtTripletStep_cff import *
 from FastSimulation.Tracking.PixelPairStep_cff import *
@@ -24,8 +27,11 @@ generalTracksBeforeMixing = RecoTracker.FinalTrackSelectors.earlyGeneralTracks_c
 
 iterTracking = cms.Sequence(
     InitialStep
-    +DetachedTripletStep
-    +LowPtTripletStep
+    +LowPtQuadStep                                                                                                                    
+    +HighPtTripletStep                                                                                                               
+    +LowPtTripletStep                                                                                                               
+    +DetachedQuadStep                                                                                                                  
+    +DetachedTripletStep 
     +PixelPairStep
     +MixedTripletStep
     +PixelLessStep
