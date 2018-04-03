8ada8a7dd3c878bec72608fc86309e8be0b3be36
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-04-03 10:20:22 +0200

    Improve printouts

--- HeterogeneousCore/AcceleratorService/test/TestAcceleratorServiceProducerGPUMock2.cc
+++ HeterogeneousCore/AcceleratorService/test/TestAcceleratorServiceProducerGPUMock2.cc
@@ -57,7 +57,7 @@ private:
   unsigned int output_;
 
   void acquire(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::acquire begin event " << iEvent.id().event() << " stream " << iEvent.streamID() << " label " << label_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << label_ << " TestAcceleratorServiceProducerGPUMock2::acquire event " << iEvent.id().event() << " stream " << iEvent.streamID();
 
     input_ = nullptr;
     if(!srcToken_.isUninitialized()) {
@@ -74,31 +74,31 @@ private:
   }
 
   void launchCPU() override {
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::launchCPU begin event " << eventId_ << " stream " << streamId_ << " label " << label_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << " " << label_ << " TestAcceleratorServiceProducerGPUMock2::launchCPU begin event " << eventId_ << " stream " << streamId_;
 
     std::random_device r;
     std::mt19937 gen(r());
     auto dist = std::uniform_real_distribution<>(1.0, 3.0); 
     auto dur = dist(gen);
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "   Task (CPU) for event " << eventId_ << " in stream " << streamId_ << " will take " << dur << " seconds";
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "  Task (CPU) for event " << eventId_ << " in stream " << streamId_ << " will take " << dur << " seconds";
     std::this_thread::sleep_for(std::chrono::seconds(1)*dur);
 
     auto input = input_ ? input_->getProduct<HeterogeneousDevice::kCPU>() : 0U;
 
     output_ = input + streamId_*100 + eventId_;
 
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::launchCPU end event " << eventId_ << " stream " << streamId_ << " label " << label_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << " " << label_ << " TestAcceleratorServiceProducerGPUMock2::launchCPU end event " << eventId_ << " stream " << streamId_;
   }
 
   void launchGPUMock(std::function<void()> callback) {
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::launchGPUMock begin event " << eventId_ << " stream " << streamId_ << " label " << label_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << " " << label_ << " TestAcceleratorServiceProducerGPUMock2::launchGPUMock begin event " << eventId_ << " stream " << streamId_;
 
     /// GPU work
     std::random_device r;
     std::mt19937 gen(r());
     auto dist = std::uniform_real_distribution<>(0.1, 1.0); 
     auto dur = dist(gen);
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "   Task (GPU) for event " << eventId_ << " in stream " << streamId_ << " will take " << dur << " seconds";
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "  " << label_ << " Task (GPU) for event " << eventId_ << " in stream " << streamId_ << " will take " << dur << " seconds";
 
     auto input = input_ ? input_->getProduct<HeterogeneousDevice::kGPUMock>() : 0U;
 
@@ -108,31 +108,32 @@ private:
                            ](){
                             std::this_thread::sleep_for(std::chrono::seconds(1)*dur);
                             gpuOutput_ = input + streamId_*100 + eventId_;
+                            edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "   " << label_ << " TestAcceleratorServiceProducerGPUMock2::launchGPUMock finished async for event " << eventId_ << " stream " << streamId_;
                             callback();
                           });
     pendingFutures.push_back(std::move(ret));
 
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::launchGPUMock end event " << eventId_ << " stream " << streamId_ << " label " << label_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << " " << label_ << " TestAcceleratorServiceProducerGPUMock2::launchGPUMock end event " << eventId_ << " stream " << streamId_;
   }
 
   void produceCPU(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::produceCPU begin event " << iEvent.id().event() << " stream " << iEvent.streamID() << " label " << label_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << label_ << " TestAcceleratorServiceProducerGPUMock2::produceCPU begin event " << iEvent.id().event() << " stream " << iEvent.streamID();
 
     iEvent.put(std::make_unique<HeterogeneousProduct>(OutputType(heterogeneous::cpuProduct(std::move(output_)))));
 
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::produceCPU end event " << iEvent.id().event() << " stream " << iEvent.streamID() << " label " << label_ << " result " << output_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << label_ << " TestAcceleratorServiceProducerGPUMock2::produceCPU end event " << iEvent.id().event() << " stream " << iEvent.streamID() << " result " << output_;
   }
 
   void produceGPUMock(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::produceGPUMock begin event " << iEvent.id().event() << " stream " << iEvent.streamID() << " label " << label_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << label_ << " TestAcceleratorServiceProducerGPUMock2::produceGPUMock begin event " << iEvent.id().event() << " stream " << iEvent.streamID();
 
     iEvent.put(std::make_unique<HeterogeneousProduct>(OutputType(heterogeneous::gpuMockProduct(std::move(gpuOutput_)),
                                                                  [this](const unsigned int& src, unsigned int& dst) {
-                                                                   edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "   Task (GPU) for event " << eventId_ << " in stream " << streamId_ << " copying to CPU";
+                                                                   edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "  " << label_ << " Task (GPU) for event " << eventId_ << " in stream " << streamId_ << " copying to CPU";
                                                                    dst = src;
-                                                                 })));
+                                                                  })));
 
-    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << "TestAcceleratorServiceProducerGPUMock2::produceGPUMock end event " << iEvent.id().event() << " stream " << iEvent.streamID() << " label " << label_ << " result " << gpuOutput_;
+    edm::LogPrint("TestAcceleratorServiceProducerGPUMock2") << label_ << " TestAcceleratorServiceProducerGPUMock2::produceGPUMock end event " << iEvent.id().event() << " stream " << iEvent.streamID() << " result " << gpuOutput_;
   }
 };
 
