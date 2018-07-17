fed38610354e723c729d11fbf2bb32e22817f6c0
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-07-17 13:45:57 +0200

    Add CPU test module

--- HeterogeneousCore/CUDACore/test/testCUDA_cfg.py
+++ HeterogeneousCore/CUDACore/test/testCUDA_cfg.py
@@ -15,17 +15,29 @@ process.options = cms.untracked.PSet(
 #process.Tracer = cms.Service("Tracer")
 
 from HeterogeneousCore.CUDACore.cudaDeviceChooser_cfi import cudaDeviceChooser
-
 process.testDevice = cudaDeviceChooser.clone()
+
+from HeterogeneousCore.CUDACore.testCUDAProducerCPU_cfi import testCUDAProducerCPU
+process.prod1cpu = testCUDAProducerCPU.clone()
+process.prod2cpu = testCUDAProducerCPU.clone(src = "prod1cpu")
+process.prod3cpu = testCUDAProducerCPU.clone(src = "prod2cpu")
+process.prod4cpu = testCUDAProducerCPU.clone(src = "prod1cpu")
+process.prod5cpu = testCUDAProducerCPU.clone()
 
 process.out = cms.OutputModule("AsciiOutputModule",
     outputCommands = cms.untracked.vstring(
-#        "keep *_testDevice_*_*"
+        "keep *_prod3cpu_*_*",
+        "keep *_prod4cpu_*_*",
+        "keep *_prod5cpu_*_*",
     ),
     verbosity = cms.untracked.uint32(0),
 )
 
-process.t = cms.Task(process.testDevice)
+
+process.t = cms.Task(
+    process.testDevice,
+    process.prod1cpu, process.prod2cpu, process.prod3cpu, process.prod4cpu, process.prod5cpu
+)
 process.p = cms.Path()
 process.p.associate(process.t)
 process.ep = cms.EndPath(process.out)
