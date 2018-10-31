2e85e4b56d2399eef6216b384f6229c0fca83808
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-10-31 22:32:30 +0100

    Simplify edmCUDAEnabled

--- HeterogeneousCore/CUDAServices/bin/edmCUDAEnabled.cpp
+++ HeterogeneousCore/CUDAServices/bin/edmCUDAEnabled.cpp
@@ -1,19 +1,7 @@
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
-#include "FWCore/ServiceRegistry/interface/ActivityRegistry.h"
-#include "HeterogeneousCore/CUDAServices/interface/CUDAService.h"
+#include <cuda_runtime.h>
 
 int main(void) {
-  edm::MessageDrop::warningAlwaysSuppressed = true;
-
-  auto desc = edm::ConfigurationDescriptions("Service", "CUDAService");
-  CUDAService::fillDescriptions(desc);
-  edm::ParameterSet ps;
-  ps.addUntrackedParameter("enabled", true);
-  desc.validate(ps, "CUDAService");
-  edm::ActivityRegistry ar;
-  auto cs = CUDAService(ps, ar);
-
-  return cs.enabled() ? 0 : 1;
+  int n;
+  auto status = cudaGetDeviceCount(&n);
+  return status == cudaSuccess ? 0 : 1;
 }
