a3bdca26c6d203164ed280b0a4ec775648eb9cb7
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-11-16 16:40:28 +0100

    Add SwitchProducer EDProducer (should have added ages ago...)

---
+++ FWCore/Modules/src/SwitchProducer.cc
@@ -0,0 +1,37 @@
+#include "FWCore/Framework/interface/global/EDProducer.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+
+namespace edm {
+  class SwitchProducer: public global::EDProducer<> {
+  public:
+    explicit SwitchProducer(ParameterSet const& iConfig);
+    ~SwitchProducer() override = default;
+
+    static void fillDescriptions(ConfigurationDescriptions& descriptions);
+    void produce(StreamID, Event& e, EventSetup const& c) const final;
+  private:
+  };
+
+  SwitchProducer::SwitchProducer(ParameterSet const& iConfig) {
+    auto const& chosenLabel = iConfig.getUntrackedParameter<std::string>("@chosen_case");
+    callWhenNewProductsRegistered([=](edm::BranchDescription const& iBranch) {
+        if(iBranch.moduleLabel() == chosenLabel) {
+          this->consumes(edm::TypeToGet{iBranch.unwrappedTypeID(),PRODUCT_TYPE},
+                         edm::InputTag{iBranch.moduleLabel(), iBranch.productInstanceName(), iBranch.processName()});
+        }
+      });
+  }
+
+  void SwitchProducer::fillDescriptions(ConfigurationDescriptions& descriptions) {
+    ParameterSetDescription desc;
+    desc.add<std::vector<std::string>>("@all_cases");
+    desc.addUntracked<std::string>("@chosen_case");
+    descriptions.addDefault(desc);
+  }
+
+  void SwitchProducer::produce(StreamID, Event& e, EventSetup const& c) const {}
+}
+
+using edm::SwitchProducer;
+DEFINE_FWK_MODULE(SwitchProducer);
