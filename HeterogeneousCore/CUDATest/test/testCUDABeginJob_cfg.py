16dc1a8eae31a744a2c22a9554888d94f22372df
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-11-16 18:48:07 +0100

    Extend test with a (disabled) filter

--- HeterogeneousCore/CUDATest/test/testCUDABeginJob_cfg.py
+++ HeterogeneousCore/CUDATest/test/testCUDABeginJob_cfg.py
@@ -9,6 +9,8 @@ process.source = cms.Source("EmptySource")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 #process.source.firstLuminosityBlock = cms.untracked.uint32(2)
 
+#process.Tracer = cms.Service("Tracer")
+
 process.options = cms.untracked.PSet(
 #    numberOfThreads = cms.untracked.uint32(4),
     numberOfStreams = cms.untracked.uint32(0)
@@ -20,8 +22,8 @@ process.options = cms.untracked.PSet(
 #     1   5
 #    / \  |
 #   2  4  6
-#   |
-#   3
+#   |     |
+#   3     7 (with a filter)
 
 process.load("HeterogeneousCore.CUDATest.prod1BeginJob_cff")
 process.load("HeterogeneousCore.CUDATest.prod5BeginJob_cff")
@@ -38,6 +40,7 @@ from HeterogeneousCore.CUDATest.testCUDAProducerGPUtoCPU_cfi import testCUDAProd
 process.prod2CUDA = testCUDAProducerGPU.clone(src = "prod1CUDA")
 process.prod3CUDA = testCUDAProducerGPU.clone(src = "prod2CUDA")
 process.prod4CUDA = testCUDAProducerGPUEW.clone(src = "prod1CUDA")
+process.prod7CUDA = testCUDAProducerGPU.clone(src = "prod6CUDA")
 
 # Switches between CPU producer and the GPU-to-CPU transfer producers
 from HeterogeneousCore.CUDATest.testCUDAProducerCPU_cfi import testCUDAProducerCPU
@@ -53,14 +56,25 @@ process.prod4 = SwitchProducer(
     cuda = testCUDAProducerGPUtoCPU.clone(src = "prod4CUDA"),
     cpu = testCUDAProducerCPU.clone(src = "prod1")
 )
+process.prod7 = SwitchProducer(
+    cuda = testCUDAProducerGPUtoCPU.clone(src = "prod7CUDA"),
+    cpu = testCUDAProducerCPU.clone(src = "prod7")
+)
+
+# Effectively a filter that always returns false
+process.prod7Filter = cms.EDFilter("ModuloEventIDFilter",
+    modulo = cms.uint32(2),
+    offset = cms.uint32(5)
+)
 
 process.out = cms.OutputModule("AsciiOutputModule",
     outputCommands = cms.untracked.vstring(
         "keep *_prod3_*_*",
         "keep *_prod4_*_*",
         "keep *_prod6_*_*",
+        "keep *_prod7_*_*",
     ),
-                               verbosity = cms.untracked.uint32(2),
+                               verbosity = cms.untracked.uint32(0),
 )
 if False:
     process.out = cms.OutputModule("PoolOutputModule",
@@ -68,6 +82,7 @@ if False:
             "keep *_prod3_*_*",
             "keep *_prod4_*_*",
             "keep *_prod6_*_*",
+            "keep *_prod7_*_*",
         ),
         fileName = cms.untracked.string("foo.root")
     )
@@ -75,7 +90,8 @@ if False:
 process.tCUDA = cms.Task(
     process.prod2CUDA,
     process.prod3CUDA,
-    process.prod4CUDA
+    process.prod4CUDA,
+    process.prod7CUDA
 )
 
 process.tSwitch = cms.Task(
@@ -90,6 +106,7 @@ process.t = cms.Task(
 )
 process.p = cms.Path(
     process.prod4
+    + process.prod7Filter + process.prod7
 )
 process.p.associate(process.t)
 process.ep = cms.EndPath(process.out)
