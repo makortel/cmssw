e62f9dcf0e4f16162a4da69f2438c4ed7112df7c
Matti Kortelainen <matti.kortelainen@cern.ch>
2018-12-10 23:43:39 +0100

    Remove CUDAToken as obsolete

--- HeterogeneousCore/CUDACore/src/classes.h
+++ HeterogeneousCore/CUDACore/src/classes.h
@@ -1,11 +1,8 @@
 #include "DataFormats/Common/interface/Wrapper.h"
-#include "HeterogeneousCore/CUDACore/interface/CUDAToken.h"
 #include "HeterogeneousCore/CUDACore/interface/CUDA.h"
 
 namespace {
   struct dictionary {
-    CUDAToken ct;
-
     // These should really be placed elsewhere?
     CUDA<float *> cf;
   };
