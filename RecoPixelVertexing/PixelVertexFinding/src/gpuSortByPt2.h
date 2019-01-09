5c17100f6d65377d5a04514441e55c8311d1a9f0
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-01-09 18:05:31 +0100

    Fix synchronization in gpuSortByPt2

--- RecoPixelVertexing/PixelVertexFinding/src/gpuSortByPt2.h
+++ RecoPixelVertexing/PixelVertexFinding/src/gpuSortByPt2.h
@@ -28,30 +28,36 @@ namespace gpuVertexFinder {
     float * __restrict__ ptv2 = data.ptv2;
     uint16_t * __restrict__ sortInd = data.sortInd;
 
-    if (nvFinal<1) return;
+    bool active = nvFinal>=1;
 
     // can be done asynchronoisly at the end of previous event
-    for (int i = threadIdx.x; i < nvFinal; i += blockDim.x) {
-      ptv2[i]=0;
+    if (active) {
+      for (int i = threadIdx.x; i < nvFinal; i += blockDim.x) {
+        ptv2[i]=0;
+      }
     }
     __syncthreads();
 
 
-    for (int i = threadIdx.x; i < nt; i += blockDim.x) {
-      if (iv[i]>9990) continue;
-      atomicAdd(&ptv2[iv[i]], ptt2[i]);
+    if (active) {
+      for (int i = threadIdx.x; i < nt; i += blockDim.x) {
+        if (iv[i]>9990) continue;
+        atomicAdd(&ptv2[iv[i]], ptt2[i]);
+      }
     }
     __syncthreads();
 
-    if (1==nvFinal) {
+    if (active && 1==nvFinal) {
       if (threadIdx.x==0) sortInd[0]=0;
-      return;
+      active = false;
     }
     __shared__ uint16_t ws[1024];
-    radixSort(ptv2,sortInd,ws,nvFinal);
+    if (active) {
+      radixSort(ptv2,sortInd,ws,nvFinal);
 
-    assert(ptv2[sortInd[nvFinal-1]]>=ptv2[sortInd[nvFinal-2]]);
-    assert(ptv2[sortInd[1]]>=ptv2[sortInd[0]]);
+      assert(ptv2[sortInd[nvFinal-1]]>=ptv2[sortInd[nvFinal-2]]);
+      assert(ptv2[sortInd[1]]>=ptv2[sortInd[0]]);
+    }
   }
 
 }
