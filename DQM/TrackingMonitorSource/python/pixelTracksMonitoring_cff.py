95aa2dbd32e6426927158522bb8f5786daf00020
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-03-12 22:26:54 +0100

    Add DQM for pixel vertices

--- DQM/TrackingMonitorSource/python/pixelTracksMonitoring_cff.py
+++ DQM/TrackingMonitorSource/python/pixelTracksMonitoring_cff.py
@@ -1,23 +1,43 @@
 import FWCore.ParameterSet.Config as cms
 
 import DQM.TrackingMonitor.TrackerCollisionTrackingMonitor_cfi
-pixelTracksMonitoring = DQM.TrackingMonitor.TrackerCollisionTrackingMonitor_cfi.TrackerCollisionTrackMon.clone()
-pixelTracksMonitoring.FolderName                = 'Tracking/PixelTrackParameters'
-pixelTracksMonitoring.TrackProducer             = 'pixelTracks'
-pixelTracksMonitoring.allTrackProducer          = 'pixelTracks'
-pixelTracksMonitoring.beamSpot                  = 'offlineBeamSpot'
-pixelTracksMonitoring.primaryVertex             = 'pixelVertices'
-pixelTracksMonitoring.pvNDOF                    = 1
-pixelTracksMonitoring.doAllPlots                = True
-pixelTracksMonitoring.doLumiAnalysis            = True
-pixelTracksMonitoring.doProfilesVsLS            = True
-pixelTracksMonitoring.doDCAPlots                = True
-pixelTracksMonitoring.doProfilesVsLS            = True
-pixelTracksMonitoring.doPlotsVsGoodPVtx         = True
-pixelTracksMonitoring.doEffFromHitPatternVsPU   = False
-pixelTracksMonitoring.doEffFromHitPatternVsBX   = False
-pixelTracksMonitoring.doEffFromHitPatternVsLUMI = False
-pixelTracksMonitoring.doPlotsVsGoodPVtx         = True
-pixelTracksMonitoring.doPlotsVsLUMI             = True
-pixelTracksMonitoring.doPlotsVsBX               = True
+pixelTracksMonitor = DQM.TrackingMonitor.TrackerCollisionTrackingMonitor_cfi.TrackerCollisionTrackMon.clone()
+pixelTracksMonitor.FolderName                = 'Tracking/PixelTrackParameters'
+pixelTracksMonitor.TrackProducer             = 'pixelTracks'
+pixelTracksMonitor.allTrackProducer          = 'pixelTracks'
+pixelTracksMonitor.beamSpot                  = 'offlineBeamSpot'
+pixelTracksMonitor.primaryVertex             = 'pixelVertices'
+pixelTracksMonitor.pvNDOF                    = 1
+pixelTracksMonitor.doAllPlots                = True
+pixelTracksMonitor.doLumiAnalysis            = True
+pixelTracksMonitor.doProfilesVsLS            = True
+pixelTracksMonitor.doDCAPlots                = True
+pixelTracksMonitor.doProfilesVsLS            = True
+pixelTracksMonitor.doPlotsVsGoodPVtx         = True
+pixelTracksMonitor.doEffFromHitPatternVsPU   = False
+pixelTracksMonitor.doEffFromHitPatternVsBX   = False
+pixelTracksMonitor.doEffFromHitPatternVsLUMI = False
+pixelTracksMonitor.doPlotsVsGoodPVtx         = True
+pixelTracksMonitor.doPlotsVsLUMI             = True
+pixelTracksMonitor.doPlotsVsBX               = True
 
+from CommonTools.ParticleFlow.goodOfflinePrimaryVertices_cfi import goodOfflinePrimaryVertices as _goodOfflinePrimaryVertices
+goodPixelVertices = _goodOfflinePrimaryVertices.clone(
+    src = "pixelVertices",
+)
+
+from DQM.TrackingMonitor.primaryVertexResolution_cfi import primaryVertexResolution as _primaryVertexResolution
+pixelVertexResolution = _primaryVertexResolution.clone(
+    vertexSrc = "goodPixelVertices",
+    rootFolder = "OfflinePixelPV/Resolution",
+)
+
+pixelTracksMonitoringTask = cms.Task(
+    goodPixelVertices
+)
+
+pixelTracksMonitoring = cms.Sequence(
+    pixelTracksMonitor +
+    pixelVertexResolution,
+    pixelTracksMonitoringTask
+)
