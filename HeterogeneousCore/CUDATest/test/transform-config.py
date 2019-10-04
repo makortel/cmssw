0c42dc562b9d2eb9d224372be66a47d67af6457f
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-10-04 21:45:32 +0200

    Add more configuration transformation options

--- HeterogeneousCore/CUDATest/test/transform-config.py
+++ HeterogeneousCore/CUDATest/test/transform-config.py
@@ -33,6 +33,18 @@ def functionCollapse(data):
                 oldop["function"] = "(collapsed)"
     return ops
 
+def functionKernelsToCPU(data):
+    for op in data:
+        if op["name"] == "kernel":
+            op["name"] = "cpu"
+    return data
+
+def functionDropMemcpy(data):
+    return [op for op in data if not "memcpy" in op["name"]]
+
+def functionDropMemset(data):
+    return [op for op in data if not "memset" in op["name"]]
+
 def transformModulePerFunction(label, module, func):
     if "acquire" in module:
         module["acquire"] = func(module["acquire"])
@@ -53,14 +65,21 @@ def transformModuleExternalWork(label, module):
         module["produce"] = []
 
 def main(opts):
-    transformModule = None
+    transformModules = []
+    if opts.externalWork:
+        transformModules.append(transformModuleExternalWork)
     if opts.mean:
-        transformModule = lambda l, m: transformModulePerFunction(l, m, functionMean)
-    elif opts.externalWork:
-        transformModule = transformModuleExternalWork
-    elif opts.collapse:
-        transformModule = lambda l, m: transformModulePerFunction(l, m, functionCollapse)
-    else:
+        transformModules.append(lambda l, m: transformModulePerFunction(l, m, functionMean))
+    if opts.kernelsToCPU:
+        transformModules.append(lambda l, m: transformModulePerFunction(l, m, functionKernelsToCPU))
+    if opts.dropMemcpy:
+        transformModules.append(lambda l, m: transformModulePerFunction(l, m, functionDropMemcpy))
+    if opts.dropMemset:
+        transformModules.append(lambda l, m: transformModulePerFunction(l, m, functionDropMemset))
+    if opts.collapse:
+        transformModules.append(lambda l, m: transformModulePerFunction(l, m, functionCollapse))
+
+    if len(transformModules) == 0:
         raise Exception("No transform operation were given")
 
     data = None
@@ -68,7 +87,8 @@ def main(opts):
         data = json.load(f)
     
     for label, module in data["moduleDefinitions"].items():
-        transformModule(label, module)
+        for func in transformModules:
+            func(label, module)
 
     with open(opts.output, "w") as f:
         json.dump(data, f, indent=2)
@@ -85,6 +105,12 @@ if __name__ == "__main__":
                         help="Make all GPU modules ExteralWork")
     parser.add_argument("--collapse", action="store_true",
                         help="Collapse all same-kind-of operations to one per module function")
+    parser.add_argument("--kernelsToCPU", action="store_true",
+                        help="Change all GPU kernels to CPU work with the same timing")
+    parser.add_argument("--dropMemcpy", action="store_true",
+                        help="Drop all memcopies")
+    parser.add_argument("--dropMemset", action="store_true",
+                        help="Drop all memsets")
 
     opts = parser.parse_args()
 
