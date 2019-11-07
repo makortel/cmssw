9b34e013365bc3ca212cc206c96214f7945efc7c
Matti Kortelainen <matti.kortelainen@cern.ch>
2019-11-07 22:23:09 +0100

    Enhance analyse-config to print out total module function timings

--- HeterogeneousCore/CUDATest/test/analyse-config.py
+++ HeterogeneousCore/CUDATest/test/analyse-config.py
@@ -4,6 +4,47 @@ import sys
 import json
 import statistics
 
+class ModuleTime:
+    def __init__(self):
+        self._cpu = 0
+        self._api = 0
+        self._kernel = 0
+
+    def cpu(self):
+        return self._cpu
+
+    def api(self):
+        return self._api
+
+    def kernel(self):
+        return self._kernel
+
+    def addCPU(self, t):
+        self._cpu += t
+
+    def addAPI(self, t):
+        self._api += t
+
+    def addKernel(self, t):
+        self._kernel += t
+
+    def add(self, mt):
+        self._cpu += mt._cpu
+        self._api += mt._api
+        self._kernel += mt._kernel
+
+def gatherModule(data):
+    t = ModuleTime()
+    for op in data:
+        avg = statistics.mean(op["values"])
+        if op["name"] == "cpu":
+            t.addCPU(avg*1e-3)
+        if op["name"] == "kernel":
+            t.addKernel(avg*1e-3)
+        if "apiTime" in op:
+            t.addAPI(statistics.mean(op["apiTime"]))
+    return t
+
 def analyseModule(data):
     for op in data:
         values = op["values"]
@@ -34,6 +75,14 @@ def analyseModule(data):
 
         print(" %s %.2f%s %s%s (%d)%s" % (op["name"], avg, stddev, unit, apiTime, len(values), metadata))
 
+def printFunction(label, name, functionTimes, totalTime):
+    ti = functionTimes[label+"_"+name]
+    print("%s::%s, cpu %.2f us (%.1f %%) api %.2f us (%.1f %%) kernel %.2f us (%.1f %%)" % (label, name,
+                                                                                      ti.cpu(), ti.cpu()/totalTime.cpu()*100,
+                                                                                      ti.api(), ti.api()/totalTime.api()*100,
+                                                                                      ti.kernel(), ti.kernel()/totalTime.kernel()*100
+                                                                                  ))
+
 def main(fname):
     data = None
     with open(fname) as f:
@@ -41,12 +90,25 @@ def main(fname):
 
     modules = data["moduleDefinitions"]
     labels = sorted(modules.keys())
+    functionTimes = {}
+    totalTime = ModuleTime()
+    for label in labels:
+        module = modules[label]
+        ti = ModuleTime()
+        if "acquire" in module:
+            ti = gatherModule(module["acquire"])
+            totalTime.add(ti)
+            functionTimes[label+"_acquire"] = ti
+        ti = gatherModule(module["produce"])
+        totalTime.add(ti)
+        functionTimes[label+"_produce"] = ti
+
     for label in labels:
         module = modules[label]
         if "acquire" in module:
-            print(label+"::acquire()")
+            printFunction(label, "acquire", functionTimes, totalTime)
             analyseModule(module["acquire"])
-        print(label+"::produce()")
+        printFunction(label, "produce", functionTimes, totalTime)
         analyseModule(module["produce"])
         print()
 
