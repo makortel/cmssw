59f3450f5ee0d1a81faf5390642d213ce46307c7
Andrea Bocci <andrea.bocci@cern.ch>
2019-11-26 17:23:43 +0100

    Add a verbose version of cudaCheck

--- HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h
+++ HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h
@@ -48,4 +48,47 @@ inline bool cudaCheck_(const char* file, int line, const char* cmd, cudaError_t 
 
 #define cudaCheck(ARG) (cudaCheck_(__FILE__, __LINE__, #ARG, (ARG)))
 
+
+namespace {
+
+  template <typename T>
+  [[noreturn]] inline void abortOnCudaErrorVerbose(
+      const char* file, int line, const char* cmd, const char* error, const char* message, T const& description) {
+    std::ostringstream out;
+    out << "\n";
+    out << file << ", line " << line << ":\n";
+    out << "cudaCheck(" << cmd << ");\n";
+    out << error << ": " << message << "\n";
+    out << description << "\n";
+    throw std::runtime_error(out.str());
+  }
+
+}  // namespace
+
+template <typename T>
+inline bool cudaCheckVerbose_(const char* file, int line, const char* cmd, CUresult result, T const& description) {
+  if (__builtin_expect(result == CUDA_SUCCESS, true))
+    return true;
+
+  const char* error;
+  const char* message;
+  cuGetErrorName(result, &error);
+  cuGetErrorString(result, &message);
+  abortOnCudaErrorVerbose(file, line, cmd, error, message, description);
+  return false;
+}
+
+template <typename T>
+inline bool cudaCheckVerbose_(const char* file, int line, const char* cmd, cudaError_t result, T const& description) {
+  if (__builtin_expect(result == cudaSuccess, true))
+    return true;
+
+  const char* error = cudaGetErrorName(result);
+  const char* message = cudaGetErrorString(result);
+  abortOnCudaErrorVerbose(file, line, cmd, error, message, description);
+  return false;
+}
+
+#define cudaCheckVerbose(ARG, DESC) (cudaCheckVerbose_(__FILE__, __LINE__, #ARG, (ARG), DESC))
+
 #endif  // HeterogeneousCore_CUDAUtilities_cudaCheck_h
