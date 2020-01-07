e718d5e16bb53e31ef0cbe77c370adc8f17f4082
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-01-07 23:01:30 +0100

    Use eventWorkHasCompleted() in EventCache

--- HeterogeneousCore/CUDAUtilities/src/EventCache.cc
+++ HeterogeneousCore/CUDAUtilities/src/EventCache.cc
@@ -3,6 +3,7 @@
 #include "HeterogeneousCore/CUDAUtilities/interface/cudaCheck.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/currentDevice.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/deviceCount.h"
+#include "HeterogeneousCore/CUDAUtilities/interface/eventWorkHasCompleted.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/ScopedSetDevice.h"
 
 namespace cudautils {
@@ -20,30 +21,24 @@ namespace cudautils {
   SharedEventPtr EventCache::get() {
     const auto dev = cudautils::currentDevice();
     auto event = makeOrGet(dev);
-    auto ret = cudaEventQuery(event.get());
-    // event is occurred, return immediately
-    if (ret == cudaSuccess) {
+    // captured work has completed, or a just-created event
+    if (eventWorkHasCompleted(event.get())) {
       return event;
     }
-    // return code is something else than "recorded", throw exception
-    if (ret != cudaErrorNotReady) {
-      cudaCheck(ret);
-    }
 
-    // Got recorded, but not yet occurred event. Try until we get an
-    // occurred event. Need to keep all recorded events until an
-    // occurred event is found in order to avoid ping-pong with a
-    // recorded event.
+    // Got an event with incomplete captured work. Try again until we
+    // get a completed (or a just-created) event. Need to keep all
+    // incomplete events until a completed event is found in order to
+    // avoid ping-pong with an incomplete event.
     std::vector<SharedEventPtr> ptrs{std::move(event)};
+    bool completed;
     do {
       event = makeOrGet(dev);
-      ret = cudaEventQuery(event.get());
-      if (ret == cudaErrorNotReady) {
+      completed = eventWorkHasCompleted(event.get());
+      if (not completed) {
         ptrs.emplace_back(std::move(event));
-      } else if (ret != cudaSuccess) {
-        cudaCheck(ret);
       }
-    } while (ret != cudaSuccess);
+    } while (not completed);
     return event;
   }
 
