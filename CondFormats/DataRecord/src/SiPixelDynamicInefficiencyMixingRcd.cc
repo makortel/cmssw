4bacc1ab72d20df29579d1da55e71ab41f675930
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-10-20 15:17:29 +0200

    Add SiPixelDynamicInefficiencyMixingRcd

---
+++ CondFormats/DataRecord/src/SiPixelDynamicInefficiencyMixingRcd.cc
@@ -0,0 +1,3 @@
+#include "CondFormats/DataRecord/interface/SiPixelDynamicInefficiencyMixingRcd.h"
+#include "FWCore/Framework/interface/eventsetuprecord_registration_macro.h"
+EVENTSETUP_RECORD_REG(SiPixelDynamicInefficiencyMixingRcd);
