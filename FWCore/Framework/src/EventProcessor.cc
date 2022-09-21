b7729bb2c3a8d9d9e6098ea3f48d550ec0a104fe
Matti Kortelainen <matti.kortelainen@cern.ch>
2022-09-21 23:50:55 +0200

    Percolate the ModuleTypeResolverBase from EventProcessor to ModuleRegistry

--- FWCore/Framework/src/EventProcessor.cc
+++ FWCore/Framework/src/EventProcessor.cc
@@ -502,7 +502,8 @@ namespace edm {
           // initialize the Schedule
           ServiceRegistry::Operate operate(serviceToken_);
           auto const& tns = ServiceRegistry::instance().get<service::TriggerNamesService>();
-          madeModules = items.initModules(*parameterSet, tns, preallocations_, &processContext_);
+          madeModules =
+              items.initModules(*parameterSet, tns, preallocations_, &processContext_, moduleTypeResolverMaker_.get());
         });
 
         group.run([&, this, tempReg]() {
@@ -596,7 +597,8 @@ namespace edm {
                                    token,
                                    serviceregistry::kConfigurationOverrides,
                                    preallocations_,
-                                   &processContext_);
+                                   &processContext_,
+                                   moduleTypeResolverMaker_);
       }
     } catch (...) {
       //in case of an exception, make sure Services are available
