97132a2e085438adc98eed07518cb969eb03b896
Matti Kortelainen <matti.kortelainen@cern.ch>
2022-09-22 21:28:50 +0200

    Cache the module makers by resolved module type
    This change allows the type resolver to return different type name per
    module instance.

--- FWCore/Framework/interface/resolveMaker.h
+++ FWCore/Framework/interface/resolveMaker.h
@@ -2,6 +2,7 @@
 #define FWCore_Framework_interface_resolveMaker_h
 
 #include "FWCore/Framework/interface/ModuleTypeResolverBase.h"
+#include "FWCore/Utilities/interface/DebugMacros.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
 #include <memory>
@@ -12,10 +13,16 @@ namespace edm::detail {
                                                 std::string const& modtype,
                                                 ModuleTypeResolverBase const* resolver);
 
-  template <typename TFactory>
+  // Returns a tuple of
+  // - non-owning pointer to the maker
+  // - boolean indicating if insertion to cache was successful
+  //  * true: insertion successful, or maker was found from the cache
+  //  * false: failed to insert the maker to cache
+  template <typename TFactory, typename TCache>
   auto resolveMaker(std::string const& moduleType,
                     ModuleTypeResolverMaker const* resolverMaker,
-                    edm::ParameterSet const& modulePSet) {
+                    edm::ParameterSet const& modulePSet,
+                    TCache& makerCache) -> std::tuple<typename TCache::mapped_type::element_type*, bool> {
     if (resolverMaker) {
       auto resolver = resolverMaker->makeResolver(modulePSet);
       auto index = resolver->kInitialIndex;
@@ -24,19 +31,38 @@ namespace edm::detail {
         auto [ttype, tindex] = resolver->resolveType(std::move(newType), index);
         newType = std::move(ttype);
         index = tindex;
+        // try the maker cache first
+        auto found = makerCache.find(newType);
+        if (found != makerCache.end()) {
+          return {found->second.get(), true};
+        }
+
+        // if not in cache, then try to create
         auto m = TFactory::get()->tryToCreate(newType);
         if (m) {
-          return m;
+          //FDEBUG(1) << "Factory:  created worker of type " << modtype << std::endl;
+          auto [it, succeeded] = makerCache.emplace(newType, std::move(m));
+          if (not succeeded) {
+            return {nullptr, false};
+          }
+          return {it->second.get(), true};
         }
+        // not found, try next one
       } while (index != resolver->kLastIndex);
       try {
         //failed to find a plugin
-        return TFactory::get()->create(moduleType);
+        auto m = TFactory::get()->create(moduleType);
+        return {nullptr, false};  // dummy return, the create() call throws an exception
       } catch (cms::Exception& iExcept) {
         detail::annotateResolverMakerExceptionAndRethrow(iExcept, moduleType, resolver.get());
       }
     }
-    return TFactory::get()->create(moduleType);
+    auto [it, succeeded] = makerCache.emplace(moduleType, TFactory::get()->create(moduleType));
+    //FDEBUG(1) << "Factory:  created worker of type " << modtype << std::endl;
+    if (not succeeded) {
+      return {nullptr, false};
+    }
+    return {it->second.get(), true};
   }
 }  // namespace edm::detail
 
