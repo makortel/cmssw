b90b0cec0830b5b3faea024173a4ca9780b5583d
Matti Kortelainen <matti.kortelainen@cern.ch>
2022-12-09 19:49:05 +0100

    AlpakaModuleTypeResolver works now

--- HeterogeneousCore/AlpakaCore/plugins/AlpakaModuleTypeResolver.cc
+++ HeterogeneousCore/AlpakaCore/plugins/AlpakaModuleTypeResolver.cc
@@ -9,18 +9,6 @@
 #include <string>
 #include <vector>
 
-namespace {
-  void ensureValidBackendName(std::string const& backend) {
-    if (not backend.empty() and backend != "serial_sync" and backend != "cuda_async") {
-      edm::Exception ex(edm::errors::Configuration);
-      ex << "AlpakaModuleTypeResolver was configured to use " << backend
-         << " backend, but it is not supported. Currently supported backends are serial_sync, cuda_async";
-      ex.addContext("Calling AlpakaModuleTypeResolverMaker constructor");
-      throw ex;
-    }
-  }
-}  // namespace
-
 class AlpakaModuleTypeResolver : public edm::ModuleTypeResolverBase {
 public:
   AlpakaModuleTypeResolver(std::string backendPrefix) : backendPrefix_(std::move(backendPrefix)) {}
@@ -30,6 +18,9 @@ public:
     constexpr auto kAlpaka = "@alpaka";
     auto found = basename.find(kAlpaka);
     if (found != std::string::npos) {
+      if (backendPrefix_.empty()) {
+        throw edm::Exception(edm::errors::LogicError) << "AlpakaModuleTypeResolver encountered a module with type name " << basename << " but the backend prefix was empty. This should not happen. Please contact framework developers.";
+      }
       basename.replace(found, std::strlen(kAlpaka), "");
       basename = backendPrefix_ + basename;
     }
@@ -45,17 +36,18 @@ public:
   AlpakaModuleTypeResolverMaker(edm::ParameterSet const& iConfig) {}
 
   std::shared_ptr<edm::ModuleTypeResolverBase const> makeResolver(edm::ParameterSet const& modulePSet) const final {
-    auto backend =
+    std::string prefix = "";
+    if (modulePSet.existsAs<edm::ParameterSet>("alpaka", false)) {
+      auto const& backend =
         modulePSet.getUntrackedParameter<edm::ParameterSet>("alpaka").getUntrackedParameter<std::string>("backend");
-    // TODO: this check is not really needed
-    ensureValidBackendName(backend);
-    auto prefix = fmt::format("alpaka_{}::", backend);
+      prefix = fmt::format("alpaka_{}::", backend);
 
-    LogDebug("AlpakaModuleTypeResolver")
+      LogDebug("AlpakaModuleTypeResolver")
         .format("AlpakaModuleTypeResolver: module {} backend prefix {}",
                 modulePSet.getParameter<std::string>("@module_label"),
                 prefix);
 
+    }
     return std::make_shared<AlpakaModuleTypeResolver>(prefix);
   }
 };
