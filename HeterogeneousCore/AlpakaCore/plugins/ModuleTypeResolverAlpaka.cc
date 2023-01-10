bada4f90a76abca3965479e64a5a91fea288dc0b
Matti Kortelainen <matti.kortelainen@cern.ch>
2023-01-10 23:38:21 +0100

    code format

--- HeterogeneousCore/AlpakaCore/plugins/ModuleTypeResolverAlpaka.cc
+++ HeterogeneousCore/AlpakaCore/plugins/ModuleTypeResolverAlpaka.cc
@@ -19,7 +19,9 @@ public:
     auto found = basename.find(kAlpaka);
     if (found != std::string::npos) {
       if (backendPrefix_.empty()) {
-        throw edm::Exception(edm::errors::LogicError) << "AlpakaModuleTypeResolver encountered a module with type name " << basename << " but the backend prefix was empty. This should not happen. Please contact framework developers.";
+        throw edm::Exception(edm::errors::LogicError)
+            << "AlpakaModuleTypeResolver encountered a module with type name " << basename
+            << " but the backend prefix was empty. This should not happen. Please contact framework developers.";
       }
       basename.replace(found, std::strlen(kAlpaka), "");
       basename = backendPrefix_ + basename;
@@ -39,14 +41,13 @@ public:
     std::string prefix = "";
     if (modulePSet.existsAs<edm::ParameterSet>("alpaka", false)) {
       auto const& backend =
-        modulePSet.getUntrackedParameter<edm::ParameterSet>("alpaka").getUntrackedParameter<std::string>("backend");
+          modulePSet.getUntrackedParameter<edm::ParameterSet>("alpaka").getUntrackedParameter<std::string>("backend");
       prefix = fmt::format("alpaka_{}::", backend);
 
       LogDebug("AlpakaModuleTypeResolver")
-        .format("AlpakaModuleTypeResolver: module {} backend prefix {}",
-                modulePSet.getParameter<std::string>("@module_label"),
-                prefix);
-
+          .format("AlpakaModuleTypeResolver: module {} backend prefix {}",
+                  modulePSet.getParameter<std::string>("@module_label"),
+                  prefix);
     }
     return std::make_shared<ModuleTypeResolverAlpaka>(prefix);
   }
