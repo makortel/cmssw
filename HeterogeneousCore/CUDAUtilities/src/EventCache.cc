c62900ef061fa84287fe44141ea1e356c5cf702f
Matti Kortelainen <matti.kortelainen@cern.ch>
2023-03-16 07:34:52 +0100

    Remove cudaEventBlockingSync flag

--- HeterogeneousCore/CUDAUtilities/src/EventCache.cc
+++ HeterogeneousCore/CUDAUtilities/src/EventCache.cc
@@ -46,7 +46,7 @@ namespace cms::cuda {
     return cache_[dev].makeOrGet([dev]() {
       cudaEvent_t event;
       // it should be a bit faster to ignore timings
-      cudaCheck(cudaEventCreateWithFlags(&event, cudaEventDisableTiming | cudaEventBlockingSync));
+      cudaCheck(cudaEventCreateWithFlags(&event, cudaEventDisableTiming));
       return std::unique_ptr<BareEvent, Deleter>(event, Deleter{dev});
     });
   }
