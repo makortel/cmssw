7a061d82585ca63d544cbf2a33f53304512097f2
Matti Kortelainen <matti.kortelainen@cern.ch>
2024-10-30 09:35:52 +0100

    Add missing files

---
+++ FWCore/PluginManager/src/PauseMaxMemoryPreloadSentry.cc
@@ -0,0 +1,16 @@
+#include "PauseMaxMemoryPreloadSentry.h"
+
+// By default do nothing, but add "hooks" that MaxMemoryPreload can
+// override with LD_PRELOAD
+void pauseMaxMemoryPreload() {}
+void unpauseMaxMemoryPreload() {}
+
+namespace edm {
+  PauseMaxMemoryPreloadSentry::PauseMaxMemoryPreloadSentry() {
+    pauseMaxMemoryPreload();
+  }
+  PauseMaxMemoryPreloadSentry::~PauseMaxMemoryPreloadSentry(){
+    unpauseMaxMemoryPreload();
+  }
+}
+
